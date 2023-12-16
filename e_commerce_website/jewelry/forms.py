0ce35bdf1e8012b9f8f008307c6c6b4cd3a7af39
Beatris Ilieve <beatrisilieve@icloud.com>
2023-12-16 11:05:02 +0200

    Added form

--- e_commerce_website/jewelry/forms.py
+++ e_commerce_website/jewelry/forms.py
@@ -1,56 +1,12 @@
 from django import forms
-from django.shortcuts import render
 
-from e_commerce_website.jewelry.models import JewelryDetails, Style, Metal, Jewelry
+from e_commerce_website.jewelry.models import Style, Metal, StoneType
 
-
-# class JewelryForm(forms.ModelForm):
-#     # styles = Style.TitleChoices.choices
-#     #
-#     # style = forms.ChoiceField(
-#     #     choices=styles,
-#     #     required=False,
-#     #     widget=forms.CheckboxSelectMultiple(),
-#     # )
-#     # class Meta:
-#         # model = JewelryDetails
-#         # fields = ('jewelry', 'metals')
-#         # widgets = {
-#         #     'jewelry': forms.CheckboxSelectMultiple(),
-#         #     'metals': forms.CheckboxSelectMultiple(),
-#         # }
-#
-#     metals = forms.ChoiceField(
-#         choices=[(metal.id, metal.title) for metal in Metal.objects.all()],
-#         required=False,
-#         label='Choose Metal',
-#         widget=forms.Select(attrs={'class': 'form-control'})  # You can add additional attributes here if needed
-#     )
-#
-#     class Meta:
-#         model = JewelryDetails
-#         # fields = ('jewelry', 'metals')
-#         fields = ('metals',)
-
-
-
-# def index_page(request):
-    
-#     if request.method == 'get':
-#         form = JewelryForm()
-        
-#     else:
-#         form = JewelryForm(request.POST)
-        
-#     context = {
-#         'form': form,
-#     }
-    
-#     return render(request, 'common/index-page.html', context)
 
 class JewelryForm(forms.Form):
-
     STYLE_CHOICES = Style.TitleChoices.choices
+    METAL_CHOICES = Metal.TitleChoices.choices
+    STONE_TYPES_CHOICES = StoneType.TitleChoices.choices
 
     style_choices = forms.MultipleChoiceField(
         choices=STYLE_CHOICES,
@@ -58,14 +14,14 @@ class JewelryForm(forms.Form):
         widget=forms.CheckboxSelectMultiple,
     )
 
-    METAL_CHOICES = Metal.TitleChoices.choices
-
     metal_choices = forms.MultipleChoiceField(
         choices=METAL_CHOICES,
         required=False,
         widget=forms.CheckboxSelectMultiple,
     )
 
-
-
-
+    stone_type_choices = forms.MultipleChoiceField(
+        choices=STONE_TYPES_CHOICES,
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+    )
