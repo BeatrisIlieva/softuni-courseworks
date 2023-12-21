5cc7c92a7092d197bebd7a547ac0f7f887a6adb5
Beatris Ilieve <beatrisilieve@icloud.com>
2023-12-21 19:12:03 +0200

    Added jewelries

--- populate_jewelries.py
+++ populate_jewelries.py
@@ -30,35 +30,53 @@ bulk_create_jewelry(
             title='Chandelier',
             quantity=10,
             price=120000.00,
-            first_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703167483/earrings/1/diamond_chandelier_earrings_eadpchsmct_e-1_lsdgdg.webp',
-            second_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703167483/earrings/1/diamond_chandelier_earrings_eadpchsmct_e-2_udb3ag.webp',
+            first_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703167483/earrings/1/diamond_chandelier_earrings.webp',
+            second_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703167483/earrings/1/diamond_chandelier_earrings_eadpchsmct.webp',
             category=categories[1],
         ),
 
         Jewelry(
             title='Queen of Diamonds',
             quantity=10,
-            price=160000.00,
-            first_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703168853/earrings/2/diamond_chandelier_earrings_eadpclafrcha_e-1_d4kztt.webp',
-            second_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703168594/earrings/2/diamond_chandelier_earrings_eadpclafrcha_e-2_ltsgnc.webp',
+            price=163000.00,
+            first_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703168853/earrings/2/diamond_chandelier_earrings_eadpclafrchaa.webp',
+            second_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703168594/earrings/2/diamond_chandelier_earrings_eadpclafrcha.webp',
             category=categories[1],
         ),
 
         Jewelry(
             title='Garland Heart',
             quantity=10,
-            price=30000.00,
-            first_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703175666/earrings/3/garland_by_harry_winston_earrings_diamond_eadphssmoc_581447_e-1_ysmxop.webp',
-            second_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703175667/earrings/3/garland_by_harry_winston_earrings_diamond_eadphssmoc_581447_e-2_z74pjd.webp',
+            price=32000.00,
+            first_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703175666/earrings/3/garland_earrings_diamond_eadphssmoc_581447_e-1_ysmxop.webp',
+            second_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703175667/earrings/3/garland_earrings_diamond_eadphssmoc_581447_e-2_z74pjd.webp',
             category=categories[1],
         ),
 
         Jewelry(
             title='Pirouette',
             quantity=10,
-            price=25000.00,
-            first_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703176025/earrings/4/pirouette_by_harry_winston_earrings__diamond__eadprfprspir_e-1_zfcw3e.webp',
-            second_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703175667/earrings/3/garland_by_harry_winston_earrings_diamond_eadphssmoc_581447_e-2_z74pjd.webp',
+            price=24000.00,
+            first_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703176025/earrings/4/pirouette_earrings__diamond__eadprfprspir_e-1_zfcw3e.webp',
+            second_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703176029/earrings/4/pirouette_earrings__diamond__eadprfprspir_e-2_qafrue.webp',
+            category=categories[1],
+        ),
+
+        Jewelry(
+            title='Lotus Cluster',
+            quantity=10,
+            price=56000.00,
+            first_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703177862/earrings/5/lotus_cluster_earrings_diamond_eadpde010ltc_e-1_dstqna.webp',
+            second_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703177862/earrings/5/lotus_cluster_earrings_diamond_eadpde010ltc_e-2_cd7sd2.webp',
+            category=categories[1],
+        ),
+
+        Jewelry(
+            title='Classics',
+            quantity=10,
+            price=97000.00,
+            first_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703178293/earrings/6/drop_earrings_diamond_and_ruby_earmrpsfpsd_431958_e-1_cycchy.webp',
+            second_image_url='https://res.cloudinary.com/deztgvefu/image/upload/v1703178292/earrings/6/drop_earrings_diamond_and_ruby_earmrpsfpsd_431958_e-2_gs6ai0.webp',
             category=categories[1],
         ),
 
@@ -85,6 +103,27 @@ bulk_create_jewelry_by_metal(
         JewelryMetal(
             jewelry=jewelries[2],
             metal=metals[3],
+        ),
+
+        JewelryMetal(
+            jewelry=jewelries[3],
+            metal=metals[3],
+        ),
+
+        JewelryMetal(
+            jewelry=jewelries[4],
+            metal=metals[3],
+        ),
+
+        JewelryMetal(
+            jewelry=jewelries[5],
+            metal=metals[3],
+        ),
+
+        JewelryMetal(
+            jewelry=jewelries[5],
+            metal=metals[0],
+            gold_carat=gold_carats[3]
         ),
 
     ]
@@ -117,5 +156,34 @@ bulk_create_jewelry_by_stone(
             stone_color=stone_colors[0],
             stone_carat=2.05,
         ),
+
+        JewelryStone(
+            jewelry=jewelries[3],
+            stone_type=stone_types[7],
+            stone_color=stone_colors[0],
+            stone_carat=4.52,
+        ),
+
+        JewelryStone(
+            jewelry=jewelries[4],
+            stone_type=stone_types[7],
+            stone_color=stone_colors[0],
+            stone_carat=4.30,
+        ),
+
+        JewelryStone(
+            jewelry=jewelries[5],
+            stone_type=stone_types[7],
+            stone_color=stone_colors[0],
+            stone_carat=2.91,
+        ),
+
+        JewelryStone(
+            jewelry=jewelries[5],
+            stone_type=stone_types[7],
+            stone_color=stone_colors[20],
+            stone_carat=2.03,
+        ),
     ]
+
 )
