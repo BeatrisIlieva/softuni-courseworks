439f705cd9c36b617025dd8189c037186654dd41
Beatris Ilieve <beatrisilieve@icloud.com>
2023-12-22 14:56:55 +0200

    Added validator

--- value_in_range_validator.py
+++ value_in_range_validator.py
@@ -1,8 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.deconstruct import deconstructible
 
 
+@deconstructible #in order to be used for models as well not only forms
 class ValueInRangeValidator:
     def __init__(self, min_value, max_value):
         self.min_value = min_value
@@ -11,6 +13,13 @@ class ValueInRangeValidator:
     def __call__(self, value):
         if value < self.min_value or value > self.max_value:
             raise ValidationError(f'Value must be between {self.min_value} and {self.max_value}')
+
+    def __eq__(self, other): #in order to be used for models as well not only forms
+        return (
+            isinstance(other, self.__class__)
+            and self.min_value == other.min_value
+            and self.max_value == other.max_value
+        )
 
 class Profile(models.Model):
     pass
