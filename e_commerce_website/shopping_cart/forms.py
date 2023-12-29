69271008dc3b0856ab77a3a555d9cfc45f290146
Beatris Ilieve <beatrisilieve@icloud.com>
2023-12-29 13:06:41 +0200

    Extended shopping cart

--- e_commerce_website/shopping_cart/forms.py
+++ e_commerce_website/shopping_cart/forms.py
@@ -1,18 +1,6 @@
-# from django import forms
-#
-# from e_commerce_website.shopping_cart.models import Order
-#
-#
-# class AddToShoppingCartForm(forms.ModelForm):
-#     class Meta:
-#         model = Order
-#         fields = ('user', 'jewelries')
-#
-#
-#     # quantity = forms.IntegerField(min_value=1)
-#
-# class RemoveFromShoppingCartForm:
-#     pass
-#
-# class FinishOrderForm:
-#     pass
+from django import forms
+
+
+class QuantityUpdateForm(forms.Form):
+    quantity = forms.IntegerField(min_value=0)
+    jewelry_id = forms.IntegerField(widget=forms.HiddenInput())
--- e_commerce_website/shopping_cart/views.py
+++ e_commerce_website/shopping_cart/views.py
@@ -1,11 +1,12 @@
 from _decimal import Decimal
 
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum, F
-from django.shortcuts import redirect
-from django.views.generic import TemplateView
+from django.db.models import Sum, F, DecimalField, ExpressionWrapper
+from django.shortcuts import redirect, render
+from django.views import View
 
 from e_commerce_website.jewelry.models import Jewelry
+from e_commerce_website.shopping_cart.forms import QuantityUpdateForm
 from e_commerce_website.shopping_cart.models import ShoppingCart
 
 
@@ -25,12 +26,10 @@ def add_to_shopping_cart(request, jewelry_pk):
     return redirect('view_shopping_cart')
 
 
-class ShoppingCartView(TemplateView):
+class ShoppingCartView(View):
     template_name = 'shopping_cart/shopping_cart.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
+    def get_context_data(self):
         if self.request.user.is_authenticated:
             user_pk = self.request.user.pk
 
@@ -43,16 +42,34 @@ class ShoppingCartView(TemplateView):
                 quantity = ShoppingCart.objects.get(jewelry_id=pk).quantity
                 jewelries_by_quantities[jewelry] = quantity
 
-            total_price_as_dict = Jewelry.objects.filter(id__in=jewelries_pks).aggregate(total_price=Sum('price'))
 
-            total_price = Decimal(total_price_as_dict['total_price'])
-            context['jewelries_by_quantities'] = jewelries_by_quantities
-            context['total_price'] = total_price
+            total_price = ShoppingCart.objects.filter(user_id=user_pk).annotate(
+                total=ExpressionWrapper(F('jewelry__price') * F('quantity'), output_field=DecimalField())
+            ).aggregate(total_sum=Sum('total')).get('total_sum') or Decimal('0.00')
+
+            context = {
+                'total_price': total_price,
+                'jewelries_by_quantities': jewelries_by_quantities,
+                'quantity_update_form': QuantityUpdateForm()
+            }
 
             return context
 
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
 
+        return render(request, self.template_name, context)
 
+    def post(self, request, *args, **kwargs):
+        quantity_update_form = QuantityUpdateForm(request.POST)
 
+        if quantity_update_form.is_valid():
+            jewelry_id = quantity_update_form.cleaned_data['jewelry_id']
+            new_quantity = quantity_update_form.cleaned_data['quantity']
 
+            ShoppingCart.objects.filter(user_id=request.user.pk, jewelry_id=jewelry_id).update(
+                quantity=new_quantity)
 
+        context = self.get_context_data()
+
+        return render(request, self.template_name, context)
