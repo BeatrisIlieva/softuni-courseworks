9c79fcd1dd5775749c39befc13579fcb2d9aa833
Beatris Ilieve <beatrisilieve@icloud.com>
2023-12-31 20:06:13 +0200

    Refactored views

--- e_commerce_website/accounts/urls.py
+++ e_commerce_website/accounts/urls.py
@@ -9,15 +9,32 @@ from e_commerce_website.accounts.views import RegisterUserView, \
     UserOrdersView
 
 urlpatterns = (
-    path('register/', RegisterUserView.as_view(), name='register_user'),
-    path('login/', LoginUserView.as_view(), name='login_user'),
-    path('logout/', LogoutUserView.as_view(), name='logout_user'),
-    path('profile/<int:pk>/', include(
-        [
-            path('', UserDetailsView.as_view(), name='details_user'),
-            path('update/', UserUpdateView.as_view(), name='update_user'),
-            path('delete/', UserDeleteView.as_view(), name='delete_user'),
-            path('my-orders/', UserOrdersView.as_view(), name='my_orders')
-        ]
-    ))
+    path(
+        'register/',
+        RegisterUserView.as_view(),
+        name='register_user'
+    ),
+    path(
+        'login/',
+        LoginUserView.as_view(),
+        name='login_user'
+    ),
+    path(
+        'logout/',
+        LogoutUserView.as_view(),
+        name='logout_user'
+    ),
+    path(
+        'profile/<int:pk>/', include(
+            [
+                path(
+                    '',
+                    UserDetailsView.as_view(),
+                    name='details_user'
+                ),
+                path('update/', UserUpdateView.as_view(), name='update_user'),
+                path('delete/', UserDeleteView.as_view(), name='delete_user'),
+                path('my-orders/', UserOrdersView.as_view(), name='my_orders')
+            ]
+        ))
 )
