138f114a87213b070ad6b4de691aea1207affd6f
Beatris Ilieve <beatrisilieve@icloud.com>
2023-12-31 20:06:50 +0200

    Refactored views

--- e_commerce_website/accounts/urls.py
+++ e_commerce_website/accounts/urls.py
@@ -32,9 +32,21 @@ urlpatterns = (
                     UserDetailsView.as_view(),
                     name='details_user'
                 ),
-                path('update/', UserUpdateView.as_view(), name='update_user'),
-                path('delete/', UserDeleteView.as_view(), name='delete_user'),
-                path('my-orders/', UserOrdersView.as_view(), name='my_orders')
+                path(
+                    'update/',
+                    UserUpdateView.as_view(),
+                    name='update_user'
+                ),
+                path(
+                    'delete/',
+                    UserDeleteView.as_view(),
+                    name='delete_user'
+                ),
+                path(
+                    'my-orders/',
+                    UserOrdersView.as_view(),
+                    name='my_orders'
+                ),
             ]
         ))
 )
