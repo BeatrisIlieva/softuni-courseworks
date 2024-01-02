ccc36773ab16c472a93ac899df53c19be4ea9f11
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-02 17:07:18 +0200

    Refactored jewelries view

--- e_commerce_website/jewelry/urls.py
+++ e_commerce_website/jewelry/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from e_commerce_website.jewelry.views import DisplayJewelriesView, JewelryDetailsView
+from e_commerce_website.jewelry.views import DisplayJewelriesView, JewelryDetailsView, DisplayJewelriesByCategoryView
 
 urlpatterns = (
-    path("categories/<int:category_pk>/", DisplayJewelriesView.as_view(), name="display_jewelries_by_category"),
+    path("categories/<int:category_pk>/", DisplayJewelriesByCategoryView.as_view(), name="display_jewelries_by_category"),
+    # path("categories/<int:category_pk>/", DisplayJewelriesView.as_view(), name="display_jewelries_by_category"),
     path("metals/<int:metal_pk>/", DisplayJewelriesView.as_view(), name="display_jewelries_by_metal"),
     path("gemstones-types/<int:stone_type_pk>/", DisplayJewelriesView.as_view(), name="display_jewelries_by_stone_type"),
     path("gemstones-colors/<int:stone_color_pk>/", DisplayJewelriesView.as_view(), name="display_jewelries_by_stone_color"),
