f29acac1a238cf32f3aba08f7245789942d949b3
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-05 23:33:02 +0200

    Refactored code

--- e_commerce_website/common/urls.py
+++ e_commerce_website/common/urls.py
@@ -11,7 +11,11 @@ urlpatterns = (
         NavigationBarView.as_view(),
         name='index_page'
     ),
-    path('search/', SearchBarView.as_view(), name='search_bar'),
+    path(
+        'search/',
+        SearchBarView.as_view(),
+        name='search_bar'
+    ),
     path('like/<int:jewelry_pk>', like_jewelry, name='like_jewelry'),
     path('likes/<int:pk>', DisplayedLikedJewelries.as_view(), name='display_liked_jewelries'),
 )
