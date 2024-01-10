83f09892d5f279dff00e0e328c4a658e362186a4
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-10 17:25:13 +0200

    Added test

--- tests/profiles/models/test_account_profile.py
+++ tests/profiles/models/test_account_profile.py
@@ -25,17 +25,23 @@ class FirstNameTests(TestCase):
             'first_name': '3eatris'
         }
 
-
     def test_create__when_first_name_contains_only_letters__expect_to_be_created(self):
-        account_profile = AccountProfile.objects.create(**self.valid_account_profile_data)
+        account_profile = AccountProfile.objects.create(
+            **self.valid_account_profile_data
+        )
+
         self.assertIsNotNone(account_profile.pk)
 
     def test_create__when_first_name_does_not_contain_only_letters__expect_to_raise(self):
         with self.assertRaises(ValidationError) as ve:
-            AccountProfile.objects.create(**self.invalid_account_profile_data).full_clean()
+            AccountProfile.objects.create(
+                **self.invalid_account_profile_data
+            ).full_clean()
 
         error_messages = ve.exception.message_dict
         actual_error_message = error_messages.get('first_name')[0]
 
-        self.assertEqual(AccountProfile.ONLY_LETTERS_EXCEPTION_MESSAGE, actual_error_message)
-
+        self.assertEqual(
+            AccountProfile.ONLY_LETTERS_EXCEPTION_MESSAGE,
+            actual_error_message
+        )
