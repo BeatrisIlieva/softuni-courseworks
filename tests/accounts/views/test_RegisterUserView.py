709f28b6a7ba9f667f91ae6b4ba312670019db14
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-11 10:50:11 +0200

    Added tests

--- tests/accounts/views/test_RegisterUserView.py
+++ tests/accounts/views/test_RegisterUserView.py
@@ -7,10 +7,16 @@ from e_commerce_website.accounts.models import AccountUser
 
 class RegisterUserViewTestCase(TestCase):
     def test_register_user_view(self):
-        AccountUser.objects.create(email='beatrisilieve@icloud.com')
+        AccountUser.objects.create(
+            email='beatrisilieve@icloud.com'
+        )
 
-        response = self.client.get(reverse('index_page'), )
+        response = self.client.get(
+            reverse('index_page'),
+        )
 
         self.assertEqual(response.status_code, 200)
 
-        self.assertTrue(get_user_model().objects.filter(email='beatrisilieve@icloud.com').exists())
+        self.assertTrue(get_user_model().objects.
+                        filter(email='beatrisilieve@icloud.com').exists()
+                        )
