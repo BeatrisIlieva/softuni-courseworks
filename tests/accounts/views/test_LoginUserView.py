2394a68681f1d698f7dc3a2a1230649da125e23e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-13 11:24:37 +0200

    Added test

--- tests/accounts/views/test_LoginUserView.py
+++ tests/accounts/views/test_LoginUserView.py
@@ -1,18 +1,23 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 
 UserModel = get_user_model()
+
+
 class LoginUserViewTests(TestCase):
     def setUp(self) -> None:
+        self.client = Client()
+
         self.user_data = {
-            'email': 'beatris@icloud.com',
-            'password': '!@123Qw'
+            'email': 'test@example.com',
+            'password': 'securepassword123'
         }
+
     def test_login_user(self):
         UserModel.objects.create_user(
             **self.user_data
         )
         self.client.login(**self.user_data)
 
-        response = self.client.get(reverse('index_page'))
+        self.client.get(reverse('index_page'))
