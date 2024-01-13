3d6f64dc208db48045e582acf1f20b5f39810767
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-13 18:27:23 +0200

    Added test

--- tests/jewelry/views/test_JewelryDetailsView.py
+++ tests/jewelry/views/test_JewelryDetailsView.py
@@ -19,24 +19,14 @@ class JewelryDetailsViewTest(TestCase):
         )
 
     def test_jewelry_details_view(self):
-        # Access the jewelry details view
         response = self.client.get(reverse('display_jewelry_details', kwargs={'pk': self.jewelry.pk}))
 
-        # Check if the response status code is 200 (OK)
         self.assertEqual(response.status_code, 200)
 
-        # Check if the correct template is used
         self.assertTemplateUsed(response, 'jewelry/jewelry_details.html')
 
-        # Check if the jewelry instance is present in the context
         self.assertEqual(response.context['object'], self.jewelry)
 
-        # You can add more specific checks based on the expected behavior of the view
-
-        # Example: Check if the form is present in the context
         self.assertIn('form', response.context)
 
-        # Example: Check if the last viewed jewelries are present in the context
         self.assertIn('last_viewed_jewelries', response.context)
-
-
