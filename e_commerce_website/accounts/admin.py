ad689e13fd82e5711a3d6c28e975d27a535058f6
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-16 21:05:38 +0200

    Updated admin

--- e_commerce_website/accounts/admin.py
+++ e_commerce_website/accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from e_commerce_website.profiles.forms import AccountProfileForm
+
 UserModel = get_user_model()
 
 
@@ -8,3 +10,34 @@ UserModel = get_user_model()
 class UserModelAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
+
+    ordering = ('email',)
+    list_display = ['email', 'last_login', ]
+    list_filter = ()
+    add_form =AccountProfileForm
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
--- e_commerce_website/inventory/admin.py
+++ e_commerce_website/inventory/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from e_commerce_website.inventory.models import Inventory
-from e_commerce_website.profiles.models import AccountProfile
+
 
 
 @admin.register(Inventory)
