a22e7d97334d1f99551f2fc6393d3c92dc8f3ec4
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-16 21:36:42 +0200

    Updated models

--- e_commerce_website/accounts/models.py
+++ e_commerce_website/accounts/models.py
@@ -1,11 +1,7 @@
 from django.contrib.auth.base_user import AbstractBaseUser
-
 from django.contrib.auth.models import PermissionsMixin
-
 from django.db import models
-
 from e_commerce_website.accounts.manager import AccountUserManager
-
 from e_commerce_website.jewelry.models import Jewelry
 
 
