e4ec5aa5a71c8ed6e0da4d1cb93f5f236fe99c1c
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-16 21:10:10 +0200

    Updated admin

--- e_commerce_website/inventory/admin.py
+++ e_commerce_website/inventory/admin.py
@@ -6,4 +6,35 @@ from e_commerce_website.inventory.models import Inventory
 
 @admin.register(Inventory)
 class Inventory(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'jewelry', 'quantity', 'price')
+
+    ordering = ('id',)
+
+    list_filter = ('price', 'quantity', 'created_at')
+
+    search_fields = ('jewelry', 'quantity', 'price')
+
+    sortable_by = ('id', 'jewelry', 'quantity', 'price', 'created_at')
+
+    fieldsets = (
+        (
+            'Title',
+            {
+                'fields': ('title',)
+            }
+        ),
+        (
+            'Image urls',
+            {
+                'fields':
+                    ('first_image_url', 'second_image_url',)
+            }
+        ),
+        (
+            'Category',
+            {
+                'fields':
+                    ('category',)
+            }
+        )
+    )
