b15bc8fd306a93112d729c9b4f9904061bd0961a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-18 19:44:56 +0200

    Updated views

--- e_commerce_website/accounts/views.py
+++ e_commerce_website/accounts/views.py
@@ -1,21 +1,16 @@
 from django.contrib.auth import login, logout
 from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect
-
 from django.urls import reverse_lazy
 from django.views import View
-
 from django.views.generic import CreateView
-
 from e_commerce_website.accounts.forms import RegisterUserForm
-
 from e_commerce_website.common.mixins import NavigationBarMixin
 
 
 class RegisterUserView(NavigationBarMixin, CreateView):
     template_name = 'accounts/register.html'
     form_class = RegisterUserForm
-
     success_url = reverse_lazy('index_page')
 
     def form_valid(self, form):
@@ -57,5 +52,3 @@ class LogoutUserView(View):
     def get(request, *args, **kwargs):
         logout(request)
         return redirect(reverse_lazy('login_user'))
-
-
