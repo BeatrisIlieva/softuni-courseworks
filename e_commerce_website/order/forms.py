c3452daf28ad426942f2cfc5dc6cc17d99498a20
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-18 19:41:13 +0200

    Updated forms

--- e_commerce_website/order/forms.py
+++ e_commerce_website/order/forms.py
@@ -1,21 +1,23 @@
 from django import forms
-
 from django.core.validators import RegexValidator
-
 from e_commerce_website.order.validators import ExpirationDateValidator
 
 class CardDetailsForm(forms.Form):
     CARD_NUMBER_LENGTH = 16
     CARD_NUMBER_REGEX_PATTERN = r'^\d{16}$'
-    CARD_NUMBER_ERROR_MESSAGE = f'The card number should be exactly {CARD_NUMBER_LENGTH} digits long.'
+    CARD_NUMBER_ERROR_MESSAGE = \
+        f'The card number should be exactly {CARD_NUMBER_LENGTH} digits long.'
 
     EXPIRATION_DATE_FORMAT = 'DD/MM'
-    EXPIRATION_DATE_FORMAT_ERROR_MESSAGE = f'The expiration date should be in the format {EXPIRATION_DATE_FORMAT}.'
-    CARD_HAS_EXPIRED_ERROR_MESSAGE = f'This card has expired.'
+    EXPIRATION_DATE_FORMAT_ERROR_MESSAGE = \
+        f'The expiration date should be in the format {EXPIRATION_DATE_FORMAT}.'
+    CARD_HAS_EXPIRED_ERROR_MESSAGE = \
+        f'This card has expired.'
 
     CVV_CODE_LENGTH = 3
     CVV_CODE_REGEX_PATTERN = r'^\d{3}$'
-    CVV_CODE_ERROR_MESSAGE = f'The CVV code should be exactly {CVV_CODE_LENGTH} digits long.'
+    CVV_CODE_ERROR_MESSAGE = \
+        f'The CVV code should be exactly {CVV_CODE_LENGTH} digits long.'
 
 
 
@@ -46,7 +48,10 @@ class CardDetailsForm(forms.Form):
         ),
         validators=[
             ExpirationDateValidator(
-                error_message=[CARD_HAS_EXPIRED_ERROR_MESSAGE, EXPIRATION_DATE_FORMAT_ERROR_MESSAGE],
+                error_message=[
+                    CARD_HAS_EXPIRED_ERROR_MESSAGE,
+                    EXPIRATION_DATE_FORMAT_ERROR_MESSAGE
+                ],
             ),
         ]
     )
