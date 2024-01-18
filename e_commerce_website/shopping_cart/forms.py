007fdc9c73d8d08c00c1d307647c22a5df96c927
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-18 19:43:25 +0200

    Updated forms

--- e_commerce_website/shopping_cart/forms.py
+++ e_commerce_website/shopping_cart/forms.py
@@ -1,26 +1,11 @@
 from django import forms
 
-from e_commerce_website.shopping_cart.models import ShoppingCart
-
 
 class QuantityUpdateForm(forms.Form):
-    quantity = forms.IntegerField(min_value=0)
-    jewelry_id = forms.IntegerField(widget=forms.HiddenInput())
+    quantity = forms.IntegerField(
+        min_value=0
+    )
 
-# class AddToCartForm(forms.Form):
-#     quantity = forms.IntegerField(initial=1, min_value=1, widget=forms.HiddenInput())
-#     pk = forms.IntegerField(widget=forms.HiddenInput())
-#
-#
-# class AddToShoppingCartForm(forms.Form):
-#     quantity = forms.IntegerField(min_value=1)
-#     jewelry_id = forms.IntegerField(widget=forms.HiddenInput())
-
-# class ShoppingCartForm(forms.ModelForm):
-#     class Meta:
-#         model = ShoppingCart
-#         fields = ['quantity']
-
-
-
-
+    jewelry_id = forms.IntegerField(
+        widget=forms.HiddenInput()
+    )
