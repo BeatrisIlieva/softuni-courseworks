2adf71fc8d5eb968af963f6fcc3751f270f54f97
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-21 14:24:22 +0200

    Refactored forms

--- e_commerce_website/accounts/forms.py
+++ e_commerce_website/accounts/forms.py
@@ -31,6 +31,10 @@ class RegisterUserForm(UserCreationForm):
         self.fields['password1'].label = False
         self.fields['password2'].label = False
 
+        self.fields['email'].help_text = None
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
+
         self.fields['email'].widget.attrs['placeholder'] = 'Enter your email'
         self.fields['password1'].widget.attrs['placeholder'] = 'Enter your password'
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm your password'
