f2baf7a061a0650acd6dd568752e03b325cae97e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-23 20:01:02 +0200

    Refactored urls

--- e_commerce_website/order/urls.py
+++ e_commerce_website/order/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from e_commerce_website.order.views import CompleteOrderView, CompleteTransactionView, OrderDetailsView, UserOrdersView
+from e_commerce_website.order.views import (
+    CompleteOrderView,
+    CompleteTransactionView,
+    OrderDetailsView,
+    UserOrdersView
+)
 
 urlpatterns = (
     path('complete-order/<int:pk>/', CompleteOrderView.as_view(), name='complete_order'),
