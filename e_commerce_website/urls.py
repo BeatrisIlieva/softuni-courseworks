212137140161a0f3f14b682f9d6f20a6d24a9863
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-23 20:03:23 +0200

    Refactored urls

--- e_commerce_website/urls.py
+++ e_commerce_website/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = (
-    path('admin/', admin.site.urls),
+    path(
+        'admin/',
+        admin.site.urls
+    ),
     path('', include('e_commerce_website.common.urls')),
     path('', include('e_commerce_website.jewelry.urls')),
     path('', include('e_commerce_website.accounts.urls')),
