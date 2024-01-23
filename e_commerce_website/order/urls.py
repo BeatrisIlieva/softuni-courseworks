65bf4e9149bba2e9016c69736b27daf975852c21
Beatris Ilieve <beatrisilieve@icloud.com>
2024-01-23 20:01:25 +0200

    Refactored urls

--- e_commerce_website/order/urls.py
+++ e_commerce_website/order/urls.py
@@ -8,8 +8,16 @@ from e_commerce_website.order.views import (
 )
 
 urlpatterns = (
-    path('complete-order/<int:pk>/', CompleteOrderView.as_view(), name='complete_order'),
-    path('complete-transaction/<int:pk>/', CompleteTransactionView.as_view(), name='complete_transaction'),
+    path(
+        'complete-order/<int:pk>/',
+        CompleteOrderView.as_view(),
+        name='complete_order'
+    ),
+    path(
+        'complete-transaction/<int:pk>/',
+        CompleteTransactionView.as_view(),
+        name='complete_transaction'
+    ),
     path('order-details/<int:pk>/', OrderDetailsView.as_view(), name='order_details'),
     path('my-orders/<int:pk>',
         UserOrdersView.as_view(),
