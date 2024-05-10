881c974d9d5bd8195a1174eb62131a0f5ac5bedd
Beatris Ilieve <beatrisilieve@icloud.com>
2024-05-10 10:24:45 +0300

    Updated Bag.js

--- client/src/components/Bag/Bag.js
+++ client/src/components/Bag/Bag.js
@@ -1,5 +1,6 @@
 import styles from "./Bag.module.css";
 import colorStyles from "../../commonCSS/Colors.module.css";
+import discoverStyles from "../Discover/Discover.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTruck } from "@fortawesome/free-solid-svg-icons";
 import { bagServiceFactory } from "../../services/bagService";
@@ -80,20 +81,29 @@ export const Bag = () => {
     }
   };
 
+  const isEmpty = bagItems.length < 1;
+
   return (
     <>
       <h2 className={styles["bag-title"]}>Your Bag</h2>
-      {bagItems.length > 0 ? (
+      {!isEmpty ? (
         <div>
           <div className={styles["bag-container"]}>
             <div className={styles["bag-left-container"]}>
               <p className={styles["bag-left-container-title"]}>
-                <FontAwesomeIcon
-                  icon={faTruck}
-                  className={colorStyles["dark-pink"]}
-                />{" "}
-                Delivery
-                {" "}
+                <span
+                  className={styles["bag-left-container-title-with-padding"]}
+                >
+                  <FontAwesomeIcon
+                    icon={faTruck}
+                    className={colorStyles["dark-pink"]}
+                  />
+                </span>
+                <span
+                  className={styles["bag-left-container-title-with-padding"]}
+                >
+                  Delivery
+                </span>
                 <span className={styles["delivery-span"]}>
                   ({totalQuantity} {totalQuantity > 1 ? "items" : "item"})
                 </span>
@@ -118,9 +128,9 @@ export const Bag = () => {
                       <ul role="list">
                         <li className={styles["bag-composition-metal"]}>
                           {item.metalInfo.map((metalItem, metalIndex) => (
-                            <div key={metalItem.metalId}>
+                            <div key={`metal_${metalItem.metalId}`}>
                               {metalItem.map((i) => (
-                                <span key={metalItem.metalId}>
+                                <span key={`metal_${metalItem.metalId}${metalIndex}`}>
                                   {i.caratWeight &&
                                     i.caratWeight.$numberDecimal &&
                                     `${i.caratWeight.$numberDecimal}ct.`}{" "}
@@ -137,9 +147,9 @@ export const Bag = () => {
                         </li>
                         <li className={styles["bag-composition-stone"]}>
                           {item.stoneInfo.map((stoneItem, stoneIndex) => (
-                            <div key={`stone_${stoneIndex}`}>
-                              {stoneItem.map((i, index) => (
-                                <span key={`stone_${stoneIndex}_${index}`}>
+                            <div key={`stone_${stoneItem.stoneId}`}>
+                              {stoneItem.map((i) => (
+                                <span key={`stone_${stoneItem.stoneId}${stoneIndex}`}>
                                   {i.caratWeight &&
                                     i.caratWeight.$numberDecimal &&
                                     `${i.caratWeight.$numberDecimal}ct.`}{" "}
@@ -272,10 +282,82 @@ export const Bag = () => {
           </div>
         </div>
       ) : (
-        <div className={styles["bag-sub-title"]}>
-          <h3>Your Shopping Bag is Empty.</h3>
-          <p>Explore and add something you love.</p>
-        </div>
+        <>
+          <div className={styles["bag-sub-title"]}>
+            <h3>Your Shopping Bag is Empty.</h3>
+            <p>Explore and add something you love.</p>
+          </div>
+          <div className={discoverStyles["discover-container"]}>
+            <div>
+              <button
+                className={`${buttonStyles["button"]} ${buttonStyles["pink"]} ${buttonStyles["hover"]}`}
+                // onClick={() => onRemove(item._id)}
+              >
+                Discover Earrings
+              </button>
+              <div className={discoverStyles["discover-image"]}>
+                <img
+                  className={discoverStyles["discover-img"]}
+                  src={
+                    "https://res.cloudinary.com/deztgvefu/image/upload/v1715272200/discover/diamond_loop_earrings_diamond_eadprddbllp_e-2_j4ugmv.webp"
+                  }
+                  alt=""
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                className={`${buttonStyles["button"]} ${buttonStyles["pink"]} ${buttonStyles["hover"]}`}
+                // onClick={() => onRemove(item._id)}
+              >
+                Discover Necklaces
+              </button>
+              <div className={discoverStyles["discover-image"]}>
+                <img
+                  className={discoverStyles["discover-img"]}
+                  src={
+                    "https://res.cloudinary.com/deztgvefu/image/upload/v1715272200/discover/sparkling_cluster_necklace_diamond_nkdpclrfspc_e-1_ztc7kf.webp"
+                  }
+                  alt=""
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                className={`${buttonStyles["button"]} ${buttonStyles["pink"]} ${buttonStyles["hover"]}`}
+                // onClick={() => onRemove(item._id)}
+              >
+                Discover Bracelets
+              </button>
+              <div className={discoverStyles["discover-image"]}>
+                <img
+                  className={discoverStyles["discover-img"]}
+                  src={
+                    "https://res.cloudinary.com/deztgvefu/image/upload/v1715272200/discover/sunflower-_bracelet_diamond_brdpnasmsf_e-1_copy_zrzmxp.webp"
+                  }
+                  alt=""
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                className={`${buttonStyles["button"]} ${buttonStyles["pink"]} ${buttonStyles["hover"]}`}
+                // onClick={() => onRemove(item._id)}
+              >
+                Discover Rings
+              </button>
+              <div className={discoverStyles["discover-image"]}>
+                <img
+                  className={discoverStyles["discover-img"]}
+                  src={
+                    "https://res.cloudinary.com/deztgvefu/image/upload/v1715272201/discover/sunflower-_ring_diamond_frdpnalgsf_e-2_pstw37.webp"
+                  }
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </>
       )}
     </>
   );
