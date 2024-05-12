d63d4e03240f729196fb3590cd47033a7858c46f
Beatris Ilieve <beatrisilieve@icloud.com>
2024-05-12 14:52:55 +0300

    Updated user registration

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -4,7 +4,6 @@ const baseUrl = "http://localhost:3030/users";
 
 export const authServiceFactory = (token) => {
   const request = requestFactory(token);
-  
 
   return {
     login: (data) => request.post(`${baseUrl}/login`, data),
@@ -12,12 +11,6 @@ export const authServiceFactory = (token) => {
     register: (data) => request.post(`${baseUrl}/register`, data),
 
     edit: (data, userId) => request.put(`${baseUrl}/profiles/${userId}`, data),
-
-    // getPersonalDetails: () =>
-    //   request.get("http://localhost:3030/profiles/get-personal-details"),
-
-    // editDeliveryDetails: (data) =>
-    //   request.put(`${baseUrl}/edit-delivery-details`, data),
 
     editEmail: (data) => request.put(`${baseUrl}/edit-email`, data),
 
