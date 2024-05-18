0017b8719a186f356eb89ef470ca991303ceecbe
Beatris Ilieve <beatrisilieve@icloud.com>
2024-05-18 17:33:09 +0300

    Updated User Details

--- client/src/components/UserDetails/UserDetails.js
+++ client/src/components/UserDetails/UserDetails.js
@@ -13,46 +13,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import { faFileInvoiceDollar } from "@fortawesome/free-solid-svg-icons";
 import { authServiceFactory } from "../../services/authService";
+import {UpdateProfile} from "./UpdateProfile/UpdateProfile"
 
-const FormKeys = {
-  FirstName: "firstName",
-  LastName: "lastName",
-  Birthday: "birthday",
-  SpecialDay: "specialDay",
-};
+
 
 export const UserDetails = () => {
-  const profileService = useService(profileServiceFactory);
   const { userId } = useContext(AuthContext);
-  const authService = useService(authServiceFactory);
+  const authService = useService(authServiceFactory)
+  const profileService = useService(profileServiceFactory)
   const [user, setUser] = useState([]);
   const [profile, setProfile] = useState([]);
 
-  const [values, setValues] = useState({
-    [FormKeys.FirstName]: { value: "", focusField: false },
-    [FormKeys.LastName]: { value: "", focusField: false },
-    [FormKeys.Birthday]: { value: "", focusField: false },
-    [FormKeys.SpecialDay]: { value: "", focusField: false },
-  });
-
-
-  useEffect(() => {
-    profileService
-      .display(userId)
-      .then((dataFromServer) => {
-        const updatedValues = { ...values };
-        for (let key in FormKeys) {
-          updatedValues[FormKeys[key]] = {
-            value: dataFromServer[FormKeys[key]],
-            focusField: true ? dataFromServer[FormKeys[key]] : false,
-          };
-        }
-        setValues(updatedValues);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
 
   useEffect(() => {
     authService
@@ -76,43 +47,7 @@ export const UserDetails = () => {
       });
   }, [profile]);
 
-  const changeHandler = (fieldKey, newValue) => {
-    setValues((prevValues) => ({
-      ...prevValues,
-      [fieldKey]: { ...prevValues[fieldKey], value: newValue },
-    }));
-  };
-
-  const onFocusField = (fieldKey) => {
-    setValues((prevValues) => ({
-      ...prevValues,
-      [fieldKey]: { ...prevValues[fieldKey], focusField: true },
-    }));
-  };
-
-  const onBlurField = () => {
-    setValues((prevValues) => {
-      const updatedValues = { ...prevValues };
-      for (let key in updatedValues) {
-        updatedValues[key].focusField = true ? values[key].value : false;
-      }
-
-      return updatedValues;
-    });
-  };
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const data = {
-      [FormKeys.FirstName]: values[FormKeys.FirstName].value,
-      [FormKeys.LastName]: values[FormKeys.LastName].value,
-      [FormKeys.Birthday]: values[FormKeys.Birthday].value,
-      [FormKeys.SpecialDay]: values[FormKeys.SpecialDay].value,
-    };
-
-    await profileService.update(userId, data);
-  };
-
+  
   const [showUpdateAddressBook, setShowUpdateAddressBook] = useState(false);
 
   const onUpdateAddressBookClick = async () => {
@@ -157,145 +92,7 @@ export const UserDetails = () => {
         <div className={styles["left-container"]}>
           <div className={styles["left-upper-sub-container"]}>
             <h3 className={styles["container-title"]}>Personal Information</h3>
-            <div className={styles["upper-sub-sub-container"]}>
-              <form
-                className={styles["profile-form-container"]}
-                method="POST"
-                onSubmit={onSubmit}
-              >
-                <div className={`${formStyles["filed-container"]}`}>
-                  <div
-                    onClick={() => onFocusField("firstName")}
-                    onBlur={onBlurField}
-                    className={formStyles["input-field-container-profile"]}
-                  >
-                    <p
-                      className={
-                        values[FormKeys.FirstName]["focusField"]
-                          ? formStyles["placeholder-on-blur"]
-                          : formStyles["placeholder"]
-                      }
-                    >
-                      First Name
-                    </p>
-                    {values[FormKeys.FirstName]["focusField"] && (
-                      <input
-                        className={formStyles["input-spot"]}
-                        type="text"
-                        name={FormKeys.FirstName}
-                        id="firstName"
-                        value={values[FormKeys.FirstName].value}
-                        onChange={(e) =>
-                          changeHandler(FormKeys.FirstName, e.target.value)
-                        }
-                        autoFocus
-                      />
-                    )}
-                  </div>
-                </div>
-                <div
-                  className={`${formStyles["filed-container"]} ${styles["input-container-left"]}`}
-                >
-                  <div
-                    onClick={() => onFocusField("lastName")}
-                    onBlur={onBlurField}
-                    className={formStyles["input-field-container-profile"]}
-                  >
-                    <p
-                      className={
-                        values[FormKeys.LastName]["focusField"]
-                          ? formStyles["placeholder-on-blur"]
-                          : formStyles["placeholder"]
-                      }
-                    >
-                      Last Name
-                    </p>
-                    {values[FormKeys.LastName]["focusField"] && (
-                      <input
-                        className={formStyles["input-spot"]}
-                        type="text"
-                        name={FormKeys.LastName}
-                        id="lastName"
-                        value={values[FormKeys.LastName].value}
-                        onChange={(e) =>
-                          changeHandler(FormKeys.LastName, e.target.value)
-                        }
-                        autoFocus
-                      />
-                    )}
-                  </div>
-                </div>
-                <div
-                  className={`${formStyles["filed-container"]} ${styles["input-container-left"]}`}
-                >
-                  <div
-                    onClick={() => onFocusField("birthday")}
-                    onBlur={onBlurField}
-                    className={formStyles["input-field-container-profile"]}
-                  >
-                    <p
-                      className={
-                        values[FormKeys.Birthday]["focusField"]
-                          ? formStyles["placeholder-on-blur"]
-                          : formStyles["placeholder"]
-                      }
-                    >
-                      Birthday (MM/DD/YYYY)
-                    </p>
-                    {values[FormKeys.Birthday]["focusField"] && (
-                      <input
-                        className={formStyles["input-spot"]}
-                        type="text"
-                        name={FormKeys.Birthday}
-                        id="birthday"
-                        value={values[FormKeys.Birthday].value}
-                        onChange={(e) =>
-                          changeHandler(FormKeys.Birthday, e.target.value)
-                        }
-                        autoFocus
-                      />
-                    )}
-                  </div>
-                </div>
-                <div
-                  className={`${formStyles["filed-container"]} ${styles["input-container-left"]}`}
-                >
-                  <div
-                    onClick={() => onFocusField("specialDay")}
-                    onBlur={onBlurField}
-                    className={formStyles["input-field-container-profile"]}
-                  >
-                    <p
-                      className={
-                        values[FormKeys.SpecialDay]["focusField"]
-                          ? formStyles["placeholder-on-blur"]
-                          : formStyles["placeholder"]
-                      }
-                    >
-                      Anniversary/Wedding (MM/DD/YYYY)
-                    </p>
-                    {values[FormKeys.SpecialDay]["focusField"] && (
-                      <input
-                        className={formStyles["input-spot"]}
-                        type="text"
-                        name={FormKeys.SpecialDay}
-                        id="specialDay"
-                        value={values[FormKeys.SpecialDay].value}
-                        onChange={(e) =>
-                          changeHandler(FormKeys.SpecialDay, e.target.value)
-                        }
-                        autoFocus
-                      />
-                    )}
-                  </div>
-                </div>
-                <input
-                  className={`${buttonStyles["button"]} ${buttonStyles["register"]} ${buttonStyles["pink"]} ${buttonStyles["hover"]}`}
-                  type="submit"
-                  value="Save"
-                />
-              </form>
-            </div>
+<UpdateProfile />
           </div>
           <div className={styles["left-bottom-sub-container"]}>
             <button
--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -4,6 +4,7 @@ const baseUrl = "http://localhost:3030/users";
 
 export const authServiceFactory = (token) => {
   const request = requestFactory(token);
+  
 
   return {
     login: (data) => request.post(`${baseUrl}/login`, data),
