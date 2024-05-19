a43b782328deb191e53062317fa47d0495ffb9f1
Beatris Ilieve <beatrisilieve@icloud.com>
2024-05-19 14:02:10 +0300

    Updated Shipping Details

--- server/src/controllers/completeOrderController.js
+++ server/src/controllers/completeOrderController.js
@@ -1,22 +1,6 @@
 const router = require("express").Router();
 const addressBookManager = require("../managers/addressBookManager");
 
-router.get("/:userId", async (req, res) => {
-  const userId = req.user._id;
-
-  try {
-    const profile = await addressBookManager.getOne(userId);
-
-    if (profile) {
-      res.status(200).json(profile);
-    }
-  } catch (err) {
-    res.status(400).json({
-      message: err.message,
-    });
-  }
-});
-
 router.put("/update/:userId", async (req, res) => {
   const userId = req.user._id;
 
