7c8ad6b1d4f88500e450b7880690841f333a0e24
Beatris Ilieve <beatrisilieve@icloud.com>
2024-05-19 14:01:49 +0300

    Updated Shipping Details

--- client/src/services/completeOrderService.js
+++ client/src/services/completeOrderService.js
@@ -5,12 +5,6 @@ const baseUrl = "http://localhost:3030/complete-order";
 export const completeOrderServiceFactory = (token) => {
   const request = requestFactory(token);
 
-  const getOne = async (userId) => {
-    const addressBook = await request.get(`${baseUrl}/${userId}`);
-
-    return addressBook;
-  };
-
   const update = async (userId, data) => {
     const addressBook = await request.put(`${baseUrl}/update/${userId}`, data);
 
