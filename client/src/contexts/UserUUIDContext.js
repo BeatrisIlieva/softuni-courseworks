c66e8d82567ff568ec408788c37a4374fb5f90c4
Beatris Ilieve <beatrisilieve@icloud.com>
2024-05-21 13:14:52 +0300

    Added UserUUIDContext

--- client/src/contexts/UserUUIDContext.js
+++ client/src/contexts/UserUUIDContext.js
@@ -1,5 +1,4 @@
-import { createContext, useContext, useEffect } from "react";
-import { useLocalStorage } from "../hooks/useLocalStorage";
+import { createContext, useContext } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export const UserUUIDContext = createContext();
