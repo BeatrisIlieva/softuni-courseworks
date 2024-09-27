bfa0e7ce8843a51ea3b8d86f9c397d8cb1a90c5f
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-27 16:58:12 +0300

    Added chanllenge

---
+++ src/stringMethods/Replace/Replace.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// The Replace() method creates a new string by replacing a specified substring with another specified substring based on the original string
+
+const sentence = "Beautiful Brazilian sky";
+
+const newSentence = sentence.replace("Brazilian", "Bulgarian");
+
+// expected Beautiful Bulgarian sky
+console.log(newSentence);
