15f1eda4ad777f3be65825d58cd912e1e5e65b54
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-27 15:03:00 +0300

    Added chanllenge

---
+++ src/pushMethod/pushMethod.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// The push() method adds one or more elements to the end of an array; Note that this method modifies the array
+
+const array = [1, 2, 3, 4, 5];
+
+const lastNumber = array.push(6, 7);
+
+console.log(array);
+console.log(array.length);
