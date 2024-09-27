8b8884e897c04829d44a4019d1ccae3a509b1f54
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-27 15:39:37 +0300

    Added chanllenge

---
+++ src/includesMethod/includesMethod.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// The includes() method determines whether an array contains a given element; it returns true or false
+
+const array = [1, 3, 4, 5];
+
+//expected true
+console.log(array.includes(5));
+
+//expected false
+console.log(array.includes(6));
