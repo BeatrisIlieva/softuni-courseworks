08d85f5639f3bb543d2d7f9c0381814a240a319b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-27 15:52:47 +0300

    Added chanllenge

---
+++ src/indexOfMethod/indexOfMethod.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// The indexOf() method returns the first index at which a given element can be found in an array; the output
+
+const animals = ["cat", "dog", "mouse"];
+
+//expected -1
+console.log(animals.indexOf("giraffe"));
+
+//expected 1
+console.log(animals.indexOf("cat"));
