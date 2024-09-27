45b9eec879d8f3bec16f61f198b2852e3cf4d73e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-27 17:07:51 +0300

    Added chanllenge

--- src/stringMethods/Replace/Replace.js
+++ src/stringMethods/Replace/Replace.js
@@ -2,9 +2,18 @@
 
 // The Replace() method creates a new string by replacing a specified substring with another specified substring based on the original string
 
-const sentence = "Beautiful Brazilian sky";
+const sentences = "Beautiful Brazilian sky";
 
-const newSentence = sentence.replace("Brazilian", "Bulgarian");
+const newSentence = sentences.replace("Brazilian", "Bulgarian");
 
 // expected Beautiful Bulgarian sky
 console.log(newSentence);
+
+const sentencesArray = ["Scary movie", "Scary story"];
+
+const newSentencesArray = sentencesArray.map(function (sentence) {
+  return sentence.replace("Scary", "Interesting");
+});
+
+// expected ["Interesting movie", "Interesting story"]
+console.log(newSentencesArray);
