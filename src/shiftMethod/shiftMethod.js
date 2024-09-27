297b4b8eda98a827eaca22bd7cadd1266cbb0d49
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-27 15:05:38 +0300

    Added chanllenge

---
+++ src/shiftMethod/shiftMethod.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// The shift() method removes the first element from an array; Note that this method modifies the array
+
+const array = [1, 2, 3, 4, 5];
+
+const firstNumber = array.shift();
+
+console.log(array);
+console.log(firstNumber);
