7aad56f46fceead27daa8c98e978c431f2ff9f53
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-27 17:12:58 +0300

    Added chanllenge

---
+++ src/stringMethods/Split/Split.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// The Split() method returns an arrays with elements in a string splitting them by a given separator
+
+const sentences = "We love JavaScript";
+
+const words = sentences.split(" ");
+
+// expected ["We", "love", "JavaScript"]
+console.log(words);
