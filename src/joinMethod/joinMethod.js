f9cdc9ecc212a23a0457c5b3799cae4ef7bdca34
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-27 15:35:08 +0300

    Added chanllenge

---
+++ src/joinMethod/joinMethod.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// The join() method returns a new string by concatenating all of the elements in an array separated by a specified string separator
+
+const array = ["Cats", "are", "amazing", "creatures", "!"];
+
+const sentence = array.join(" ");
+
+// expected "Cats are amazing creatures!"
+
+console.log(sentence);
