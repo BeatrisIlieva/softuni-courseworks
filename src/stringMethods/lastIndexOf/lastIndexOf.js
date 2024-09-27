7fda25010e24eefeacf023f462919bb282cc45bd
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-27 16:49:46 +0300

    Added chanllenge

---
+++ src/stringMethods/lastIndexOf/lastIndexOf.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// The indexOf() method returns the starting index at which a searched string is found; if a search string is not found, it returns -1
+
+const sentence = "Beautiful Brazilian sky";
+
+const foundIndex = sentence.lastIndexOf("B");
+
+// expected 10
+console.log(foundIndex);
