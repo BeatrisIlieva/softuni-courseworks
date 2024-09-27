1f9bb6bea58a5dbfe95675b168357a4116c1e2ee
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-27 14:59:56 +0300

    Added chanllenge

---
+++ src/popMethod/popMethod.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Removes the last element from an array and returns it; Note that this method modifies the array
+
+const array = [1, 2, 3, 4, 5];
+
+const lastNumber = array.pop();
+
+console.log(array);
+console.log(lastNumber);
