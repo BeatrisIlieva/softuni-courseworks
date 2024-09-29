3d1bc1649cd82b6fef601f57329c681b712d6829
Beatris Ilieve <beatrisilieve@icloud.com>
2024-09-29 16:34:27 +0300

    Added chanlenge

--- challenges.js
+++
@@ -1,7 +0,0 @@
-import { sourceCode } from "./src/arrayMethods/filterMethod/filterMethod.js";
-
-
-const challenges = [
-    [sourceCode, selectionOptions, firstChallengeDescription],
-    [sourceCode, selectionOptions, secondChallengeDescription],
-  ];
