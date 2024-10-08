1849c5017493376838a4cd722f9dce83bcee12e4
Beatris Ilieve <beatrisilieve@icloud.com>
2024-10-08 13:03:33 +0300

    Added solution

--- first_steps_in_oop/01.rhombus_of_stars.py
+++ first_steps_in_oop/01.rhombus_of_stars.py
@@ -1,13 +1,16 @@
-n = int(input())
-
-
 def print_row(count, n):
     print(" " * (n - count), end="")
     print(" ".join(["*"] * count))
 
 
-for count in range(1, n + 1):
-    print_row(count, n)
+def print_rhombus(n):
+    for count in range(1, n + 1):
+        print_row(count, n)
 
-for count in range(n - 1, 0, -1):
-    print_row(count, n)
+    for count in range(n - 1, 0, -1):
+        print_row(count, n)
+
+
+n = int(input())
+
+print_rhombus(n)
