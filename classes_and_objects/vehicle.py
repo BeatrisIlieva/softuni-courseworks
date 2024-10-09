0925022ae4c1e36353327f549f1e876cd84a39eb
Beatris Ilieve <beatrisilieve@icloud.com>
2024-10-09 15:59:34 +0300

    Added solution

--- classes_and_objects/vehicle.py
+++ classes_and_objects/vehicle.py
@@ -1,5 +1,5 @@
 class Vehicle:
-    def __init__(self, mileage: int, max_speed: int = 150):
+    def __init__(self, mileage: int, max_speed: int=150):
         self.mileage = mileage
         self.max_speed = max_speed
         self.gadgets = []
