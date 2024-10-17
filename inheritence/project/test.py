1971a12cba8ec4886c39d0ede17e6bb217c35d30
Beatris Ilieve <beatrisilieve@icloud.com>
2024-10-17 17:18:51 +0300

    Added solution

---
+++ inheritence/project/test.py
@@ -0,0 +1,4 @@
+from project.fruit import Fruit
+
+f = Fruit("10/10/2029", "banana")
+print(f.__dict__)
---
+++ test.js
@@ -0,0 +1,11 @@
+
+
+function change_result(){
+    a = 6
+    console.log(a)
+}
+
+let a = 5
+
+change_result()
+console.log(a)
