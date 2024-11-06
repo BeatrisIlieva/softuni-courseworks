c8efd18065396e3c543fa79dfd68d8361fc62edd
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-06 15:00:52 +0200

    Added solution

--- recursion_and_backtracking/exe/2.nested_loops_to_recursion.py
+++ recursion_and_backtracking/exe/2.nested_loops_to_recursion.py
@@ -1,13 +1,13 @@
-def nested_loops_to_recursion(n, vector, idx):
+def nested_loops_to_recursion(n, array, idx):
     if idx >= n:
-        print(" ".join([str(x) for x in vector]))
+        print(" ".join([str(x) for x in array]))
         return
 
     for num in range(1, n + 1):
-        vector[idx] = num
-        nested_loops_to_recursion(n, vector, idx + 1)
+        array[idx] = num
+        nested_loops_to_recursion(n, array, idx + 1)
 
 
 n = int(input())
-vector = [None] * n
-nested_loops_to_recursion(n, vector, 0)
+array = [None] * n
+nested_loops_to_recursion(n, array, 0)
