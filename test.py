98ec8e8a554b6030eaeefc33297b63b006ac6d78
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-08 16:21:03 +0200

    Added solution

--- test.py
+++ test.py
@@ -1,23 +1,24 @@
-def binary_search(array, target):
-    left_idx = 0
-    right_idx = len(array) - 1
+def binary_search(array, target, left_idx, right_idx):
+    if left_idx > right_idx:
+        return -1
 
-    while left_idx <= right_idx:
-        mid_idx = (left_idx + right_idx) // 2
-        mid_el = array[mid_idx]
+    mid_idx = (left_idx + right_idx) // 2
+    mid_el = array[mid_idx]
 
-        if target == mid_el:
-            return mid_idx
+    if target == mid_el:
+        return mid_idx
 
-        if target < mid_el:
-            right_idx -= 1
-        else:
-            left_idx += 1
+    if target < mid_el:
+        return binary_search(array, target, left_idx, right_idx - 1)
 
-    return -1
+    if target > mid_el:
+        return binary_search(array, target, left_idx + 1, right_idx)
 
 
 array = [int(x) for x in input().split()]
 target = int(input())
 
-print(binary_search(array, target))
+left_idx = 0
+right_idx = len(array) - 1
+
+print(binary_search(array, target, left_idx, right_idx))
