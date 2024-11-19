0c979cc054324af496089a384dbc8c5ba2e70990
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-19 15:11:48 +0200

    added

---
+++ data_operations_in_django_with_queries/caller.py
@@ -0,0 +1,14 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+from main_app.models import Pet
+
+def create_pet(name: str, species: str):
+    pet = Pet.objects.create(name=name, species=species)
+
+    return f"{pet.name} is a very cute {pet.species}!"
+
