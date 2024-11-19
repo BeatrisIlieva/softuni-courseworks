e17901831ceedbf9c6a0c95a7c9f2dd7db882563
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-19 15:19:02 +0200

    added

--- data_operations_in_django_with_queries/caller.py
+++ data_operations_in_django_with_queries/caller.py
@@ -5,10 +5,28 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import Pet
+from main_app.models import Pet, Artifact
+
 
 def create_pet(name: str, species: str):
     pet = Pet.objects.create(name=name, species=species)
 
     return f"{pet.name} is a very cute {pet.species}!"
 
+
+def create_artifact(
+    name: str, origin: str, age: int, description: str, is_magical: bool
+):
+    artifact = Artifact.objects.create(
+        name=name,
+        origin=origin,
+        age=age,
+        description=description,
+        is_magical=is_magical,
+    )
+    
+    return f"The artifact {artifact.name} is {artifact.age} years old!"
+
+def delete_all_artifacts():
+    Artifact.objects.all().delete()
+    
