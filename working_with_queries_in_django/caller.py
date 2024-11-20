76e7ea4b893c85f3634c5d048607ba1ad7f0643e
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-20 12:45:41 +0200

    added

--- working_with_queries_in_django/caller.py
+++ working_with_queries_in_django/caller.py
@@ -9,7 +9,7 @@ from typing import List
 
 from django.db.models import Q, F, Value, When, Case
 
-from main_app.models import ArtworkGallery, Laptop
+from main_app.models import ArtworkGallery, Laptop, ChessPlayer
 
 
 def show_highest_rated_art():
@@ -60,3 +60,49 @@ def update_operation_systems():
 
 def delete_inexpensive_laptops():
     Laptop.objects.filter(price__lt=1200).delete()
+
+
+def bulk_create_chess_players(args: List[ChessPlayer]):
+    ChessPlayer.objects.bulk_create(args)
+
+
+def delete_chess_players():
+    ChessPlayer.objects.filter(
+        title=ChessPlayer._meta.get_field("title").default
+    ).delete()
+
+
+def change_chess_games_won():
+    ChessPlayer.objects.filter(title="GM").update(games_won=30)
+
+
+def change_chess_games_lost():
+    ChessPlayer.objects.filter(
+        title=ChessPlayer._meta.get_field("title").default
+    ).update(games_lost=25)
+
+
+def change_chess_games_drawn():
+    ChessPlayer.objects.update(games_drawn=10)
+
+
+def grand_chess_title_GM():
+    ChessPlayer.objects.filter(rating__gte=2400).update(title="GM")
+
+
+def grand_chess_title_IM():
+    ChessPlayer.objects.filter(Q(rating__gte=2300) & Q(rating__lte=2399)).update(
+        title="IM"
+    )
+
+
+def grand_chess_title_FM():
+    ChessPlayer.objects.filter(Q(rating__gte=2200) & Q(rating__lte=2299)).update(
+        title="FM"
+    )
+
+
+def grand_chess_title_regular_player():
+    ChessPlayer.objects.filter(Q(rating__gte=0) & Q(rating__lte=2199)).update(
+        title="regular player"
+    )
