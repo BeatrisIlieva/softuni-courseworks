f47a1447b52924f6d4c42a272e5853a77d7444ac
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-20 17:12:48 +0200

    added

--- django_models_relations/caller.py
+++ django_models_relations/caller.py
@@ -24,7 +24,7 @@ def show_all_authors_with_their_books():
 
     queryset = Book.objects.values("author__name").annotate(
         books=StringAgg("title", delimiter=", ")
-    )
+    ).order_by("author_id")
 
     for el in queryset:
         print(f"{el['author__name']} has written - {el['books']}!")
@@ -32,4 +32,17 @@ def show_all_authors_with_their_books():
 
 def delete_all_authors_without_books():
     Author.objects.filter(book__isnull=True).delete()
+
+
+# author1 = Author.objects.create(name="J.K. Rowling")
+# author2 = Author.objects.create(name="George Orwell")
+# author3 = Author.objects.create(name="Harper Lee")
+# author4 = Author.objects.create(name="Mark Twain") 
+# book1 = Book.objects.create(title="Harry Potter and the Philosopher's Stone",price=19.99,author=author1)
+# book2 = Book.objects.create(title="1984",price=14.99,author=author2)
+# book3 = Book.objects.create(title="To Kill a Mockingbird",price=12.99,author=author3)
+authors_with_books = show_all_authors_with_their_books()
+print(authors_with_books)
+delete_all_authors_without_books()
+print(Author.objects.count())
 
