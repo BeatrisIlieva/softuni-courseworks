57341c6df7adb7544190947c4c5176de88431c83
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-21 14:31:15 +0200

    added

--- models_inheritance_and_customization/caller.py
+++ models_inheritance_and_customization/caller.py
@@ -4,24 +4,23 @@ import django
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
-from main_app.models import UserProfile, Message, Student
+from main_app.models import UserProfile, Message, Student, CreditCard, Hotel, Room, RegularReservation, SpecialReservation
+from datetime import date
+from django.core.exceptions import ValidationError
 
-# student1 = Student(name="Alice", student_id=45.23)
-# student1.full_clean()
-# student1.save()
-# retrieved_student1 = Student.objects.filter(name="Alice").first()
+# hotel = Hotel.objects.create(name="Hotel ABC", address="123 Main St")
+# room1 = Room.objects.create(    hotel=hotel,    number="102",    capacity=2,    total_guests=1,    price_per_night=100.00)
+# special_reservation1 = SpecialReservation(    room=room1,    start_date=date(2023, 1, 1),    end_date=date(2023, 1, 5))
+# print(special_reservation1.save())
+# special_reservation2 = SpecialReservation(    room=room1,    start_date=date(2023, 1, 10),    end_date=date(2023, 1, 12))
+# print(special_reservation2.save())
 
-# # Print the saved ID of the student1
-# print(retrieved_student1.student_id)
+# print(special_reservation1.calculate_total_cost())
 
-# # Try to parse zero as ID and expect ValueError
-# try:
-#     student2 = Student(name="Bob", student_id="0")
-#     student2.full_clean()
-#     student2.save()
-# except ValueError as error:
-#     print(error)
+# print(special_reservation1.reservation_period())
 
-
-
-
+# special_reservation1 = SpecialReservation.objects.get(pk=)
+# try:    
+#     special_reservation1.extend_reservation(5)
+# except ValidationError as e:    
+#     print(e)
--- models_inheritance_and_customization/main_app/models.py
+++ models_inheritance_and_customization/main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from datetime import timedelta
+from decimal import Decimal
 
 
 class UserProfile(models.Model):
@@ -62,7 +64,7 @@ class StudentIDField(models.PositiveIntegerField):
         try:
             return int(value)
         except ValueError:
-            return "Invalid input for student ID"
+            raise ValueError("Invalid input for student ID")
 
     def get_prep_value(self, value):
         cleaned_value = self.to_python(value)
@@ -79,3 +81,145 @@ class Student(models.Model):
     )
 
     student_id = StudentIDField()
+
+
+class MaskedCreditCardField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs["max_length"] = 20
+        super().__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not isinstance(value, str):
+            raise ValidationError("The card number must be a string")
+
+        if not value.isdigit():
+            raise ValidationError("The card number must contain only digits")
+
+        if not len(value) == 16:
+            raise ValidationError("The card number must be exactly 16 characters long")
+
+        return f"****-****-****-{value[-4:]}"
+
+
+class CreditCard(models.Model):
+    card_owner = models.CharField(
+        max_length=100,
+    )
+
+    card_number = MaskedCreditCardField()
+
+
+class Hotel(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    address = models.CharField(
+        max_length=200,
+    )
+
+
+class Room(models.Model):
+    hotel = models.ForeignKey(
+        to=Hotel,
+        on_delete=models.CASCADE,
+    )
+
+    number = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    capacity = models.PositiveIntegerField()
+
+    total_guests = models.PositiveIntegerField()
+
+    price_per_night = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    def clean(self):
+        if self.capacity < self.total_guests:
+            raise ValidationError("Total guests are more than the capacity of the room")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+
+        super().save(*args, **kwargs)
+
+        return f"Room {self.number} created successfully"
+
+
+class BaseReservation(models.Model):
+    class Meta:
+        abstract = True
+
+    room = models.ForeignKey(
+        to=Room,
+        on_delete=models.CASCADE,
+    )
+
+    start_date = models.DateField()
+
+    end_date = models.DateField()
+
+    def reservation_period(self):
+        return (self.end_date - self.start_date).days
+
+    def calculate_total_cost(self):
+        cost = self.reservation_period() * self.room.price_per_night
+        return f"{cost:.2f}"
+
+
+class RegularReservation(BaseReservation):
+    def clean(self):
+        if self.start_date >= self.end_date:
+            raise ValidationError("Start date cannot be after or in the same end date")
+
+        dates_overlapping = RegularReservation.objects.filter(
+            start_date__gte=self.start_date, end_date__lte=self.end_date
+        )
+
+        if dates_overlapping:
+            raise ValidationError(f"Room {self.room.number} cannot be reserved")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+
+        super().save(*args, **kwargs)
+
+        return f"Regular reservation for room {self.room.number}"
+
+
+class SpecialReservation(BaseReservation):
+    def clean(self):
+        if self.start_date >= self.end_date:
+            raise ValidationError("Start date cannot be after or in the same end date")
+
+        dates_overlapping = SpecialReservation.objects.filter(
+            start_date__gte=self.start_date, end_date__lte=self.end_date
+        )
+
+        if dates_overlapping:
+            raise ValidationError(f"Room {self.room.number} cannot be reserved")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+
+        super().save(*args, **kwargs)
+
+        return f"Special reservation for room {self.room.number}"
+    
+    def extend_reservation(self, days: int):
+        end_date = self.end_date + timedelta(days=days)
+        
+        dates_overlapping = SpecialReservation.objects.filter(start_date__range=[self.start_date, end_date])
+        
+        if dates_overlapping:
+            raise ValidationError("Error during extending reservation")
+        
+        self.end_date = end_date
+        super().save()
+        
+        return f"Extended reservation for room {self.room.number} with {days} days"
