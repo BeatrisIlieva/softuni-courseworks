c52fe18ea9cbafefd269c02506231fa0f4be064b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-21 19:47:06 +0200

    Updated models

---
+++ django_ecommerce_strategy_pattern/product/management/commands/initialize_products_data.py
@@ -0,0 +1,55 @@
+import os
+import django
+from django.core.management.base import BaseCommand
+
+from django_ecommerce_strategy_pattern.product import (
+    Size,
+    Category,
+    CategorySize,
+    Color,
+    Product,
+)
+
+
+class Command(BaseCommand):
+    help = "Initialize data for your Django app"
+
+    def handle(self, *args, **options):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_gems.settings")
+        django.setup()
+
+        self.stdout.write(self.style.SUCCESS("Starting data initialization..."))
+
+        self.bulk_create_size()
+        
+        self.bulk_create_category()
+        
+        self.stdout.write(self.style.SUCCESS('Data initialization completed successfully.'))
+
+    def bulk_create_size(self):
+        Size.objects.bulk_create(
+            [
+                Size(measurement=4.05),
+                Size(measurement=4.98),
+                Size(measurement=5.86),
+                Size(measurement=15.02),
+                Size(measurement=17.08),
+                Size(measurement=19.03),
+                Size(measurement=40.64),
+                Size(measurement=43.18),
+                Size(measurement=45.72),
+                Size(measurement=4.7),
+                Size(measurement=4.9),
+                Size(measurement=5.05),
+            ]
+        )
+        
+
+    
+    def bulk_create_category(self):        
+        Category.objects.bulk_create([
+            Category(title="E",),
+            Category(title="B"),
+            Category(title="N"),
+            Category(title="R"),
+        ])
