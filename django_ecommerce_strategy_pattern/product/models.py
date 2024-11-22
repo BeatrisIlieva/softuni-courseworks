15f810319a0757f565f80f7ea1b025aa94a5868b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-22 15:25:13 +0200

    Updated models

--- django_ecommerce_strategy_pattern/product/models.py
+++ django_ecommerce_strategy_pattern/product/models.py
@@ -1,139 +1,19 @@
 from django.db import models
 
 
-class EarringSize(models.Model):
-    
-    MEASUREMENT_CHOICES = (
-        (4.05, 4.05),
-        (4.98, 4.98),
-        (5.86, 5.86),
-    )
+# class Category(models.Model):
 
-    measurement = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        choices=MEASUREMENT_CHOICES,
-    )
+#     TITLE_CHOICES = (
+#         ("E", "Earrings"),
+#         ("B", "Bracelets"),
+#         ("N", "Necklaces"),
+#         ("R", "Rings"),
+#     )
 
-
-class BraceletSize(models.Model):
-    
-    MEASUREMENT_CHOICES = (
-        (15.02, 15.02),
-        (17.08, 17.08),
-        (19.03, 19.03),
-    )
-
-    measurement = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        choices=MEASUREMENT_CHOICES,
-    )
-
-
-class NecklaceSize(models.Model):
-    
-    MEASUREMENT_CHOICES = (
-        (40.64, 40.64),
-        (43.18, 43.18),
-        (45.72, 45.72),
-    )
-
-    measurement = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        choices=MEASUREMENT_CHOICES,
-    )
-
-
-class RingSize(models.Model):
-    
-    MEASUREMENT_CHOICES = (
-        (4.07, 4.07),
-        (4.09, 4.09),
-        (5.05, 5.05),
-    )
-
-    measurement = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        choices=MEASUREMENT_CHOICES,
-    )
-
-
-class EarringPrice(models.Model):
-    
-    PRICE_CHOICES = (
-        (43_000.00, 43_000.00),
-        (44_000.00, 44_000.00),
-        (45_000.00, 45_000.00),
-    )
-
-    price = models.DecimalField(
-        max_digits=7,
-        decimal_places=2,
-        choices=PRICE_CHOICES,
-    )
-
-
-class BraceletPrice(models.Model):
-    
-    PRICE_CHOICES = (
-        (34_000.00, 34_000.00),
-        (35_000.00, 35_000.00),
-        (36_000.00, 36_000.00),
-    )
-
-    price = models.DecimalField(
-        max_digits=7,
-        decimal_places=2,
-        choices=PRICE_CHOICES,
-    )
-
-
-class NecklacePrice(models.Model):
-    
-    PRICE_CHOICES = (
-        (55_000.00, 55_000.00),
-        (56_000.00, 56_000.00),
-        (57_000.00, 57_000.00),
-    )
-
-    price = models.DecimalField(
-        max_digits=7,
-        decimal_places=2,
-        choices=PRICE_CHOICES,
-    )
-
-
-class RingPrice(models.Model):
-    
-    PRICE_CHOICES = (
-        (23_000.00, 23_000.00),
-        (24_000.00, 24_000.00),
-        (25_000.00, 25_000.00),
-    )
-    
-    price = models.DecimalField(
-        max_digits=7,
-        decimal_places=2,
-        choices=PRICE_CHOICES,
-    )
-
-
-class Category(models.Model):
-
-    TITLE_CHOICES = (
-        ("E", "Earrings"),
-        ("B", "Bracelets"),
-        ("N", "Necklaces"),
-        ("R", "Rings"),
-    )
-
-    title = models.CharField(
-        max_length=15,
-        choices=TITLE_CHOICES,
-    )
+#     title = models.CharField(
+#         max_length=15,
+#         choices=TITLE_CHOICES,
+#     )
 
 
 class Color(models.Model):
@@ -154,18 +34,6 @@ class BaseProduct(models.Model):
     class Meta:
         abstract = True
 
-    category = models.ForeignKey(
-        to=Category,
-        on_delete=models.CASCADE,
-        related_name="category",
-    )
-
-    color = models.ForeignKey(
-        to=Color,
-        on_delete=models.CASCADE,
-        related_name="color",
-    )
-
     description = models.TextField()
 
     first_image_url = models.URLField()
@@ -176,56 +44,108 @@ class BaseProduct(models.Model):
 
 
 class Earring(BaseProduct):
-    size = models.ForeignKey(
-        to=EarringSize,
-        on_delete=models.CASCADE,
-        related_name="earring_size",
+
+    SIZE_CHOICES = (
+        (4.05, 4.05),
+        (4.98, 4.98),
+        (5.86, 5.86),
     )
 
-    price = models.ForeignKey(
-        to=EarringPrice,
-        on_delete=models.CASCADE,
-        related_name="earring_price",
+    PRICE_CHOICES = (
+        (43_000.00, 43_000.00),
+        (44_000.00, 44_000.00),
+        (45_000.00, 45_000.00),
     )
+
+    size = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        choices=SIZE_CHOICES,
+    )
+
+    price = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        choices=PRICE_CHOICES,
+    )
+
+    color = models.ForeignKey(to=Color, on_delete=models.CASCADE, related_name="color")
 
 
 class Bracelet(BaseProduct):
-    size = models.ForeignKey(
-        to=BraceletSize,
-        on_delete=models.CASCADE,
-        related_name="bracelet_size",
+    SIZE_CHOICES = (
+        (15.02, 15.02),
+        (17.08, 17.08),
+        (19.03, 19.03),
     )
 
-    price = models.ForeignKey(
-        to=BraceletPrice,
-        on_delete=models.CASCADE,
-        related_name="bracelet_price",
+    PRICE_CHOICES = (
+        (34_000.00, 34_000.00),
+        (35_000.00, 35_000.00),
+        (36_000.00, 36_000.00),
+    )
+
+    size = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        choices=SIZE_CHOICES,
+    )
+
+    price = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        choices=PRICE_CHOICES,
     )
 
 
 class Necklace(BaseProduct):
-    size = models.ForeignKey(
-        to=NecklaceSize,
-        on_delete=models.CASCADE,
-        related_name="necklace_size",
+    SIZE_CHOICES = (
+        (40.64, 40.64),
+        (43.18, 43.18),
+        (45.72, 45.72),
     )
 
-    price = models.ForeignKey(
-        to=NecklacePrice,
-        on_delete=models.CASCADE,
-        related_name="necklace_price",
+    PRICE_CHOICES = (
+        (55_000.00, 55_000.00),
+        (56_000.00, 56_000.00),
+        (57_000.00, 57_000.00),
+    )
+
+    size = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        choices=SIZE_CHOICES,
+    )
+
+    price = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        choices=PRICE_CHOICES,
     )
 
 
 class Ring(BaseProduct):
-    size = models.ForeignKey(
-        to=RingSize,
-        on_delete=models.CASCADE,
-        related_name="ring_size",
+
+    SIZE_CHOICES = (
+        (4.07, 4.07),
+        (4.09, 4.09),
+        (5.05, 5.05),
     )
 
-    price = models.ForeignKey(
-        to=RingPrice,
-        on_delete=models.CASCADE,
-        related_name="ring_price",
+    PRICE_CHOICES = (
+        (23_000.00, 23_000.00),
+        (24_000.00, 24_000.00),
+        (25_000.00, 25_000.00),
+    )
+
+    size = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        choices=SIZE_CHOICES,
+    )
+
+    price = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        choices=PRICE_CHOICES,
     )
