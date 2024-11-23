9939cbff46bf990b76fb706603bb2e1256f230a8
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-23 16:45:02 +0200

    Updated models

--- django_ecommerce_strategy_pattern/product/management/commands/initialize_products_data.py
+++ django_ecommerce_strategy_pattern/product/management/commands/initialize_products_data.py
@@ -62,37 +62,30 @@ class Command(BaseCommand):
                 Description(
                     content="A medley of marquise, pear-shaped, and round brilliant diamonds, weighing a total of approximately 4.38 carats, set in platinum."
                 ),
-                
                 Description(
                     content="45 pear-shaped and round brilliant sapphires weighing a total of approximately 4.36 carats and 33 pear-shaped, marquise and round brilliant diamonds weighing a total of approximately 4.24 carats, set in platinum."
                 ),
-                
                 Description(
                     content="45 pear-shaped and round brilliant sapphires weighing a total of approximately 4.17 carats and 33 pear-shaped, marquise and round brilliant diamonds weighing a total of approximately 4.24 carats, set in platinum."
                 ),
-                
                 Description(
                     content="78 pear-shaped, marquise, and round brilliant diamonds, weighing a total of approximately 7.46 carats, set in platinum."
                 ),
-                
                 Description(
                     content="78 pear-shaped and round brilliant sapphires weighing a total of approximately 8.61 carats and 99 marquise and round brilliant diamonds weighing a total of approximately 8.60 carats, set in platinum."
                 ),
                 Description(
                     content="78 pear-shaped and round brilliant sapphires weighing a total of approximately 8.61 carats and 99 marquise and round brilliant diamonds weighing a total of approximately 8.37 carats, set in platinum."
                 ),
-                
                 Description(
                     content="177 pear-shaped, marquise, and round brilliant diamonds, weighing a total of approximately 15.35 carats, set in platinum."
                 ),
                 Description(
                     content="6 pear-shaped sapphires weighing a total of approximately 2.22 carats and 1 round brilliant diamond weighing approximately 0.05 carats, set in platinum."
                 ),
-                
                 Description(
                     content="6 pear-shaped sapphires weighing a total of approximately 2.15 carats and 1 round brilliant diamond weighing approximately 0.05 carats, set in platinum."
                 ),
-
                 Description(
                     content="6 pear-shaped and 1 round brilliant diamond, weighing a total of approximately 1.66 carats, set in platinum."
                 ),
@@ -127,7 +120,6 @@ class Command(BaseCommand):
                     color=colors[2],
                     description=descriptions[2],
                 ),
-                
                 Product(
                     first_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714894/forget-me-not-collection/bracelets/forget_me_not_bracelet_diamond_and_pink_sapphire_brpsprfflrfmn_e_1_vz9pv4.avif",
                     second_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714893/forget-me-not-collection/bracelets/forget_me_not_bracelet_diamond_and_pink_sapphire_brpsprfflrfmn_e_2_kdpnm6.avif",
@@ -149,7 +141,6 @@ class Command(BaseCommand):
                     color=colors[2],
                     description=descriptions[5],
                 ),
-                
                 Product(
                     first_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714890/forget-me-not-collection/necklaces/forget_me_not_lariat_necklace_diamond_and_pink_sapphire_nkpspltflrfmn_e_1_kuxbds.webp",
                     second_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714890/forget-me-not-collection/necklaces/forget_me_not_lariat_necklace_diamond_and_pink_sapphire_nkpspltflrfmn_e_2_d2fc78.webp",
@@ -157,7 +148,6 @@ class Command(BaseCommand):
                     color=colors[0],
                     description=descriptions[6],
                 ),
-                
                 Product(
                     first_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714891/forget-me-not-collection/necklaces/forget_me_not_lariat_necklace_diamond_and_sapphire_nkspltflrfmn_e_1_p2uxlj.webp",
                     second_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714890/forget-me-not-collection/necklaces/forget_me_not_lariat_necklace_diamond_and_sapphire_nkspltflrfmn_e_2_hxgdcy.avif",
@@ -172,7 +162,6 @@ class Command(BaseCommand):
                     color=colors[2],
                     description=descriptions[8],
                 ),
-                
                 Product(
                     first_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714892/forget-me-not-collection/rings/forget_me_not_ring_diamond_and_pink_sapphire_frpsprfflrfmn_e_1_qfumu3.webp",
                     second_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714892/forget-me-not-collection/rings/forget_me_not_ring_diamond_and_pink_sapphire_frpsprfflrfmn_e_2_k7nhpe.avif",
@@ -180,7 +169,6 @@ class Command(BaseCommand):
                     color=colors[0],
                     description=descriptions[9],
                 ),
-
                 Product(
                     first_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714893/forget-me-not-collection/rings/forget_me_not_ring_diamond_and_sapphire_frsprfflrfmn_e_1_pm9u6t.avif",
                     second_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714891/forget-me-not-collection/rings/forget_me_not_ring_diamond_and_sapphire_frsprfflrfmn_e_2_ucppcd.avif",
@@ -188,7 +176,6 @@ class Command(BaseCommand):
                     color=colors[1],
                     description=descriptions[10],
                 ),
-
                 Product(
                     first_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714891/forget-me-not-collection/rings/forget_me_not_diamond_ring_frdprfflrfmn_e-1h_yueh2k.webp",
                     second_image_url="https://res.cloudinary.com/deztgvefu/image/upload/v1723714891/forget-me-not-collection/rings/forget_me_not_diamond_ring_frdprfflrfmn_e-2h_mktny9.webp",
@@ -196,6 +183,5 @@ class Command(BaseCommand):
                     color=colors[2],
                     description=descriptions[11],
                 ),
-                
             ]
         )
