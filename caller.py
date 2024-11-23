83385f0e09a5e11e144af9dc8166c3ba9d0c3c51
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-23 15:15:25 +0200

    Updated models

--- caller.py
+++ caller.py
@@ -7,7 +7,26 @@ django.setup()
 
 from django_ecommerce_strategy_pattern.product.models import Product
 
-result = Product.objects.filter(category__pk=1).select_related("category")
+result = Product.objects.filter(category__pk=1).select_related("category", "description", "color").prefetch_related("inventory")
 
 for product in result:
     print(product.category.get_title_display())
+    print(product.description.content)
+    print(product.color.get_title_display())
+    print(product.first_image_url)
+    for inventory in product.inventory.all():
+        print(inventory.price)
+        print(inventory.size)
+        print(inventory.quantity)
+        
+# result = Product.objects.filter(category__pk=1)
+
+# for product in result:
+#     print(product.category.get_title_display())
+#     print(product.description.content)
+#     print(product.color.get_title_display())
+#     print(product.first_image_url)
+#     for inventory in product.inventory.all():
+#         print(inventory.price)
+#         print(inventory.size)
+#         print(inventory.quantity)
