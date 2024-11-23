0302208414ccb4fef65016babc16b4e3043bf452
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-23 16:45:17 +0200

    Updated models

--- django_ecommerce_strategy_pattern/inventory/management/commands/initialize_inventory_data.py
+++ django_ecommerce_strategy_pattern/inventory/management/commands/initialize_inventory_data.py
@@ -30,30 +30,54 @@ class Command(BaseCommand):
         self.stdout.write(
             self.style.SUCCESS("Data initialization completed successfully.")
         )
-        
+
     def bulk_create_size(self):
         categories = Category.objects.all()
-        
+
         Size.objects.bulk_create(
             [
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][0], category=categories[0]),
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][1], category=categories[0]),
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][2], category=categories[0]),
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][0], category=categories[1]),
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][1], category=categories[1]),
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][2], category=categories[1]),
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][0], category=categories[2]),
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][1], category=categories[2]),
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][2], category=categories[2]),
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][0], category=categories[3]),
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][1], category=categories[3]),
-                Size(measurement=Size.MEASUREMENT_CHOICES[0][2], category=categories[3]),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][0], category=categories[0]
+                ),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][1], category=categories[0]
+                ),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][2], category=categories[0]
+                ),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][0], category=categories[1]
+                ),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][1], category=categories[1]
+                ),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][2], category=categories[1]
+                ),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][0], category=categories[2]
+                ),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][1], category=categories[2]
+                ),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][2], category=categories[2]
+                ),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][0], category=categories[3]
+                ),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][1], category=categories[3]
+                ),
+                Size(
+                    measurement=Size.MEASUREMENT_CHOICES[0][2], category=categories[3]
+                ),
             ]
         )
-        
+
     def bulk_create_price(self):
         sizes = Size.objects.all()
-        
+
         Price.objects.bulk_create(
             [
                 Price(amount=Price.AMOUNT_CHOICES[0][0], size=sizes[0]),
@@ -78,44 +102,113 @@ class Command(BaseCommand):
 
         Inventory.objects.bulk_create(
             [
-                Inventory(quantity=3, product=products[0], size=sizes[0], price=prices[0]),
-                Inventory(quantity=3, product=products[0], size=sizes[1], price=prices[1]),
-                Inventory(quantity=3, product=products[0], size=sizes[2], price=prices[2]),
-                Inventory(quantity=3, product=products[1], size=sizes[0], price=prices[0]),
-                Inventory(quantity=3, product=products[1], size=sizes[1], price=prices[1]),
-                Inventory(quantity=3, product=products[1], size=sizes[2], price=prices[2]),
-                Inventory(quantity=3, product=products[2], size=sizes[0], price=prices[0]),
-                Inventory(quantity=3, product=products[2], size=sizes[1], price=prices[1]),
-                Inventory(quantity=3, product=products[2], size=sizes[2], price=prices[2]),
-                
-                Inventory(quantity=3, product=products[3], size=sizes[3], price=prices[3]),
-                Inventory(quantity=3, product=products[3], size=sizes[4], price=prices[4]),
-                Inventory(quantity=3, product=products[3], size=sizes[5], price=prices[5]),
-                Inventory(quantity=3, product=products[4], size=sizes[3], price=prices[3]),
-                Inventory(quantity=3, product=products[4], size=sizes[4], price=prices[4]),
-                Inventory(quantity=3, product=products[4], size=sizes[5], price=prices[5]),
-                Inventory(quantity=3, product=products[5], size=sizes[3], price=prices[3]),
-                Inventory(quantity=3, product=products[5], size=sizes[4], price=prices[4]),
-                Inventory(quantity=3, product=products[5], size=sizes[5], price=prices[5]),
-                
-                Inventory(quantity=3, product=products[6], size=sizes[6], price=prices[6]),
-                Inventory(quantity=3, product=products[6], size=sizes[7], price=prices[7]),
-                Inventory(quantity=3, product=products[6], size=sizes[8], price=prices[8]),
-                Inventory(quantity=3, product=products[7], size=sizes[6], price=prices[6]),
-                Inventory(quantity=3, product=products[7], size=sizes[7], price=prices[7]),
-                Inventory(quantity=3, product=products[7], size=sizes[8], price=prices[8]),
-                Inventory(quantity=3, product=products[8], size=sizes[6], price=prices[6]),
-                Inventory(quantity=3, product=products[8], size=sizes[7], price=prices[7]),
-                Inventory(quantity=3, product=products[8], size=sizes[8], price=prices[8]),
-                
-                Inventory(quantity=3, product=products[9], size=sizes[9], price=prices[9]),
-                Inventory(quantity=3, product=products[9], size=sizes[10], price=prices[10]),
-                Inventory(quantity=3, product=products[9], size=sizes[11], price=prices[11]),
-                Inventory(quantity=3, product=products[10], size=sizes[9], price=prices[9]),
-                Inventory(quantity=3, product=products[10], size=sizes[10], price=prices[10]),
-                Inventory(quantity=3, product=products[10], size=sizes[11], price=prices[11]),
-                Inventory(quantity=3, product=products[11], size=sizes[9], price=prices[9]),
-                Inventory(quantity=3, product=products[11], size=sizes[10], price=prices[10]),
-                Inventory(quantity=3, product=products[11], size=sizes[11], price=prices[11]),
+                Inventory(
+                    quantity=3, product=products[0], size=sizes[0], price=prices[0]
+                ),
+                Inventory(
+                    quantity=3, product=products[0], size=sizes[1], price=prices[1]
+                ),
+                Inventory(
+                    quantity=3, product=products[0], size=sizes[2], price=prices[2]
+                ),
+                Inventory(
+                    quantity=3, product=products[1], size=sizes[0], price=prices[0]
+                ),
+                Inventory(
+                    quantity=3, product=products[1], size=sizes[1], price=prices[1]
+                ),
+                Inventory(
+                    quantity=3, product=products[1], size=sizes[2], price=prices[2]
+                ),
+                Inventory(
+                    quantity=3, product=products[2], size=sizes[0], price=prices[0]
+                ),
+                Inventory(
+                    quantity=3, product=products[2], size=sizes[1], price=prices[1]
+                ),
+                Inventory(
+                    quantity=3, product=products[2], size=sizes[2], price=prices[2]
+                ),
+                Inventory(
+                    quantity=3, product=products[3], size=sizes[3], price=prices[3]
+                ),
+                Inventory(
+                    quantity=3, product=products[3], size=sizes[4], price=prices[4]
+                ),
+                Inventory(
+                    quantity=3, product=products[3], size=sizes[5], price=prices[5]
+                ),
+                Inventory(
+                    quantity=3, product=products[4], size=sizes[3], price=prices[3]
+                ),
+                Inventory(
+                    quantity=3, product=products[4], size=sizes[4], price=prices[4]
+                ),
+                Inventory(
+                    quantity=3, product=products[4], size=sizes[5], price=prices[5]
+                ),
+                Inventory(
+                    quantity=3, product=products[5], size=sizes[3], price=prices[3]
+                ),
+                Inventory(
+                    quantity=3, product=products[5], size=sizes[4], price=prices[4]
+                ),
+                Inventory(
+                    quantity=3, product=products[5], size=sizes[5], price=prices[5]
+                ),
+                Inventory(
+                    quantity=3, product=products[6], size=sizes[6], price=prices[6]
+                ),
+                Inventory(
+                    quantity=3, product=products[6], size=sizes[7], price=prices[7]
+                ),
+                Inventory(
+                    quantity=3, product=products[6], size=sizes[8], price=prices[8]
+                ),
+                Inventory(
+                    quantity=3, product=products[7], size=sizes[6], price=prices[6]
+                ),
+                Inventory(
+                    quantity=3, product=products[7], size=sizes[7], price=prices[7]
+                ),
+                Inventory(
+                    quantity=3, product=products[7], size=sizes[8], price=prices[8]
+                ),
+                Inventory(
+                    quantity=3, product=products[8], size=sizes[6], price=prices[6]
+                ),
+                Inventory(
+                    quantity=3, product=products[8], size=sizes[7], price=prices[7]
+                ),
+                Inventory(
+                    quantity=3, product=products[8], size=sizes[8], price=prices[8]
+                ),
+                Inventory(
+                    quantity=3, product=products[9], size=sizes[9], price=prices[9]
+                ),
+                Inventory(
+                    quantity=3, product=products[9], size=sizes[10], price=prices[10]
+                ),
+                Inventory(
+                    quantity=3, product=products[9], size=sizes[11], price=prices[11]
+                ),
+                Inventory(
+                    quantity=3, product=products[10], size=sizes[9], price=prices[9]
+                ),
+                Inventory(
+                    quantity=3, product=products[10], size=sizes[10], price=prices[10]
+                ),
+                Inventory(
+                    quantity=3, product=products[10], size=sizes[11], price=prices[11]
+                ),
+                Inventory(
+                    quantity=3, product=products[11], size=sizes[9], price=prices[9]
+                ),
+                Inventory(
+                    quantity=3, product=products[11], size=sizes[10], price=prices[10]
+                ),
+                Inventory(
+                    quantity=3, product=products[11], size=sizes[11], price=prices[11]
+                ),
             ]
         )
