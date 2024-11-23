ffeb007ba569dd2baf4d2fc0ded89d3efad03e30
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-23 19:02:19 +0200

    Updated models

--- caller.py
+++ caller.py
@@ -7,30 +7,40 @@ os.environ.setdefault(
 )
 django.setup()
 
+from abc import ABC, abstractmethod
+from enum import Enum
 from django_ecommerce_strategy_pattern.product.models import Product
+from django_ecommerce_strategy_pattern.product.strategies import get_entity_details, FiltrationMethod
 
-result = Product.objects.get_product_entity_full_details(1, 1)
+# result = Product.objects.get_product_entity_full_details(1, 1)
 
 
-for product in result:
-    print(f"Product: {product}")
-    print(f"Category: {product.category.get_title_display()}")
-    print(f"Description: {product.description.content}")
-    print(f"Color: {product.color.get_title_display()}")
-    
-    print(f"Min Price: {product.min_price}")
-    print(f"Max Price: {product.max_price}")
+# for product in result:
+#     print(f"Product: {product}")
+#     print(f"Category: {product.category.get_title_display()}")
+#     print(f"Description: {product.description.content}")
+#     print(f"Color: {product.color.get_title_display()}")
 
-    for inventory in product.product_inventory.all():
-        print(f"  Inventory Quantity: {inventory.quantity}")
-        print(f"  Size Code: {inventory.size.measurement}")  # The size code, e.g., "ES"
-        print(
-            f"  Size Measurement: {inventory.size.get_measurement_display()}"
-        )  # The full measurement, e.g., "4.05"
-        print(f"  Price Code: {inventory.price.amount}")  # The price code, e.g., "ES"
-        print(
-            f"  Price Amount: {inventory.price}"
-        )  # The full price, e.g., "43,000.00"
-        print(f"  Is Sold Out: {'Yes' if inventory.is_sold_out else 'No'}")
-        print(f"Quantity: {inventory.quantity}")
+#     print(f"Min Price: {product.min_price}")
+#     print(f"Max Price: {product.max_price}")
+
+#     for inventory in product.product_inventory.all():
+#         print(f"  Inventory Quantity: {inventory.quantity}")
+#         print(f"  Size Code: {inventory.size.measurement}")  # The size code, e.g., "ES"
+#         print(
+#             f"  Size Measurement: {inventory.size.get_measurement_display()}"
+#         )  # The full measurement, e.g., "4.05"
+#         print(f"  Price Code: {inventory.price.amount}")  # The price code, e.g., "ES"
+#         print(
+#             f"  Price Amount: {inventory.price}"
+#         )  # The full price, e.g., "43,000.00"
+#         print(f"  Is Sold Out: {'Yes' if inventory.is_sold_out else 'No'}")
+#         print(f"Quantity: {inventory.quantity}")
+
+
+
+
+
+print(get_entity_details(1, 2, FiltrationMethod.SHORT_DETAILS))
+
 
--- django_ecommerce_strategy_pattern/product/strategies.py
+++ django_ecommerce_strategy_pattern/product/strategies.py
@@ -18,12 +18,41 @@ class FiltrationStrategy(ABC):
 
 class ShortEntityDetails(FiltrationStrategy):
     def get_entity_details(self, category_pk, color_pk):
-        return Product.objects.get_product_entity(category_pk, color_pk)
+        result = Product.objects.get_product_entity_short_details(category_pk, color_pk)
+        
+        for product in result:
+            print(f"Product: {product}")
+            print(f"Category: {product.category.get_title_display()}")
+            print(f"Color: {product.color.get_title_display()}")
+
+            print(f"Min Price: {product.min_price}")
+            print(f"Max Price: {product.max_price}")
+
+            for inventory in product.product_inventory.all():
+                print(f"  Inventory Quantity: {inventory.quantity}")
+                print(f"  Size Measurement: {inventory.size.get_measurement_display()}")
+                print(f"  Price Amount: {inventory.price}")
+                print(f"  Is Sold Out: {'Yes' if inventory.is_sold_out else 'No'}")
 
 
 class FullEntityDetails(FiltrationStrategy):
     def get_entity_details(self, category_pk, color_pk):
-        return Product.objects.get_product_entity(category_pk, color_pk)
+        result =  Product.objects.get_product_entity_full_details(category_pk, color_pk)
+        
+        for product in result:
+            print(f"Product: {product}")
+            print(f"Category: {product.category.get_title_display()}")
+            print(f"Description: {product.description.content}")
+            print(f"Color: {product.color.get_title_display()}")
+
+            print(f"Min Price: {product.min_price}")
+            print(f"Max Price: {product.max_price}")
+
+            for inventory in product.product_inventory.all():
+                print(f"  Inventory Quantity: {inventory.quantity}")
+                print(f"  Size Measurement: {inventory.size.get_measurement_display()}")
+                print(f"  Price Amount: {inventory.price}")
+                print(f"  Is Sold Out: {'Yes' if inventory.is_sold_out else 'No'}")
 
 
 class FiltrationContext:
@@ -42,6 +71,3 @@ def get_entity_details(category_pk, color_pk, method):
 
     context = FiltrationContext(strategy=strategies[method])
     return context.get_entity_details(category_pk, color_pk)
-
-
-print(get_entity_details(1, 2, FiltrationMethod.SHORT_DETAILS))
