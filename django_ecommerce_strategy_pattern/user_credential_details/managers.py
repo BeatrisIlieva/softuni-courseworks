62c85a4c28f109a13351d7d28ffa3f89ccdde4fa
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-24 10:42:25 +0200

    updated models

--- django_ecommerce_strategy_pattern/user_credential_details/managers.py
+++ django_ecommerce_strategy_pattern/user_credential_details/managers.py
@@ -12,11 +12,13 @@ class UserCredentialDetailsManager(BaseUserManager):
         user = self.model(email=email, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
+        
         return user
 
     def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
+        
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
