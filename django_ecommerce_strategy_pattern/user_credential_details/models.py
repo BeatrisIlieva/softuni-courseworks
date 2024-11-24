59766eee6c027487d1a80e85544112d2761518a8
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-24 10:41:45 +0200

    updated models

--- django_ecommerce_strategy_pattern/user_credential_details/models.py
+++ django_ecommerce_strategy_pattern/user_credential_details/models.py
@@ -1,8 +1,21 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-class UserCredentialDetails(models.Model):
+from django_ecommerce_strategy_pattern.user_credential_details.managers import (
+    UserCredentialDetailsManager,
+)
+
+
+class UserCredentialDetails(AbstractBaseUser, PermissionsMixin):
+    USERNAME_FIELD = "email"
+
+    objects = UserCredentialDetailsManager()
+
     email = models.EmailField(
         unique=True,
     )
-    
-    password = 
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
