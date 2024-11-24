94dd6d1645fd16b8f05c91ace2bc7889b5b145d4
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-24 15:02:40 +0200

    updated models

--- caller.py
+++ caller.py
@@ -1,23 +1,18 @@
 import os
 import django
 
-from django.db import models
-from django.core.validators import RegexValidator
-
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", "django_ecommerce_strategy_pattern.settings"
 )
 django.setup()
 
-from django_ecommerce_strategy_pattern.user_credential_details.models import UserCredentialDetails
+from django_ecommerce_strategy_pattern.user_credential_details.models import (
+    UserCredentialDetails,
+)
+
 # from django_ecommerce_strategy_pattern.user_shipping_details.models import UserShippingDetails
 # from django_ecommerce_strategy_pattern.user_payment_details.models import UserPaymentDetails
-
-
-
 
 
 # from django_ecommerce_strategy_pattern.product.strategies import (
@@ -28,10 +23,4 @@ from django_ecommerce_strategy_pattern.user_credential_details.models import Use
 
 # print(get_entity_details(2, 2, FiltrationMethod.FULL_DETAILS))
 
-user_cr = UserCredentialDetails.objects.create(email="bea8@icloud.com")
-
-
-
-
-
-
+# user_cr = UserCredentialDetails.objects.create(email="bea8@icloud.com")
