fd0ccf5594f045ca1ae864a862f9d983728efdf1
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-26 15:15:59 +0200

    updated models

--- django_ecommerce_strategy_pattern/common/utils.py
+++ django_ecommerce_strategy_pattern/common/utils.py
@@ -1,17 +1,16 @@
-from django.core.exceptions import ValidationError
 from django.core.validators import (
     RegexValidator,
     MinLengthValidator,
-    MaxLengthValidator,
 )
+
 from django.db import models
 
 
-def create_custom_char_field(
+def create_char_field(
     max_length,
     min_length,
     pattern,
-    regex_error_message,
+    pattern_error_message,
     min_length_error_message,
     max_length_error_message,
 ):
@@ -24,7 +23,7 @@ def create_custom_char_field(
         validators=[
             RegexValidator(
                 regex=pattern,
-                message=regex_error_message,
+                message=pattern_error_message,
             ),
             MinLengthValidator(
                 limit_value=min_length,
