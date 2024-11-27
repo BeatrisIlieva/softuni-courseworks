a4c256ae2170e5aab6020c758cbe5ba10751e2de
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-27 18:29:39 +0200

    updated models

---
+++ django_ecommerce_strategy_pattern/product/factories/__init__.py
@@ -0,0 +1,8 @@
+from .product_factory import (
+    AbstractProductFactory,
+    ProductFactory,
+    Earring,
+    Bracelet,
+    Necklace,
+    Ring,
+)
---
+++ django_ecommerce_strategy_pattern/product/models/__init__.py
@@ -0,0 +1,18 @@
+from .color import (
+    Color,
+)
+from .description import (
+    Description,
+)
+from .image_url import (
+    BaseImageUrl,
+    FirstImageUrl,
+    SecondImageUrl,
+)
+from .product import (
+    BaseProduct,
+    Earring,
+    Bracelet,
+    Necklace,
+    Ring,
+)
