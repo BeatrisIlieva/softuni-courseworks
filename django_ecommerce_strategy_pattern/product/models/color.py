df853f3148019b0e9401d2673cc2600a14516f63
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-27 18:07:50 +0200

    updated models

--- django_ecommerce_strategy_pattern/product/models/color.py
+++ django_ecommerce_strategy_pattern/product/models/color.py
@@ -1,3 +1,6 @@
+from django.db import models
+
+
 class Color(models.Model):
     TITLE_CHOICES = (
         ("P", "Pink"),
--- django_ecommerce_strategy_pattern/product/models/description.py
+++ django_ecommerce_strategy_pattern/product/models/description.py
@@ -1,3 +1,6 @@
+from django.db import models
+
+
 class Description(models.Model):
     content = models.TextField(
         max_length=300,
