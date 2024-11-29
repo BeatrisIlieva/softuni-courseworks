7b98781155c76848e6b979990977661aec1eba46
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-29 15:58:30 +0200

    updated views

--- django_ecommerce_strategy_pattern/order/facade.py
+++ django_ecommerce_strategy_pattern/order/facade.py
@@ -4,6 +4,10 @@ from django_ecommerce_strategy_pattern.user_shipping_details.models import (
     UserShippingDetails,
 )
 
+from .strategies import (
+    execute_context,
+)
+
 
 class UpdateDeliveryAddress:
     def get_user_details(self, user):
@@ -19,14 +23,14 @@ class UpdateDeliveryAddress:
         user_sd.street_address = input("Street Address*: ")
         user_sd.apartment = input("Apartment: ")
         user_sd.postal_code = input("Postal Code*: ")
-        
+
         return self.__save_user_details(user_sd)
 
     def __save_user_details(self, user_sd):
         try:
             user_sd.full_clean()
             user_sd.save()
-            
+
             return True
 
         except ValidationError as e:
@@ -34,13 +38,15 @@ class UpdateDeliveryAddress:
 
 
 class SetDeliveryMethod:
-    pass
+    def get_user_preferred_delivery_method(user, method):
+        return execute_context(user, method)
+
 
 class Facade:
     def __init__(
         self,
         update_delivery_address: UpdateDeliveryAddress,
-        set_delivery_method,
+        set_delivery_method: SetDeliveryMethod,
         execute_payment,
         create_order,
         delete_shopping_bag,
@@ -57,10 +63,14 @@ class Facade:
         has_provided_delivery_details = self._update_delivery_address.get_user_details()
         has_selected_delivery_method = self._set_delivery_method()
         has_successfully_processed_payment = self._execute_payment()
-        
-        if (has_provided_delivery_details and has_selected_delivery_method and has_successfully_processed_payment):
+
+        if (
+            has_provided_delivery_details
+            and has_selected_delivery_method
+            and has_successfully_processed_payment
+        ):
             self._create_order()
             self._delete_shopping_bag()
             self._send_confirmation_email()
-        
+
         return None
--- django_ecommerce_strategy_pattern/order/strategies.py
+++ django_ecommerce_strategy_pattern/order/strategies.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
 from enum import Enum
 from decimal import Decimal
+from datetime import timedelta
 
-from django.core.exceptions import ValidationError
+from django.utils.timezone import now
+
 
 from django_ecommerce_strategy_pattern.shopping_bag.models import (
     ShoppingBag,
@@ -17,24 +19,24 @@ class DeliveryMethod(Enum):
 
 class DeliveryStrategy(ABC):
     @abstractmethod
-    def calculate_delivery_cost(self) -> float:
+    def calculate_delivery_cost(self, user=None) -> float:
         pass
 
     @abstractmethod
-    def get_delivery_details(self) -> str:
+    def calculate_delivery_due_date(self) -> str:
         pass
 
 
 class StorePickupStrategy(DeliveryStrategy):
-    def calculate_delivery_cost(self) -> float:
-        return 0.0
+    def calculate_delivery_cost(self, user=None) -> float:
+        return Decimal(0)
 
-    def get_delivery_details(self, user) -> str:
-        return "Pickup from the nearest store location."
+    def calculate_delivery_due_date(self) -> str:
+        return now().date()
 
 
 class ExpressHomeDeliveryStrategy(DeliveryStrategy):
-    def calculate_delivery_cost(self, user) -> float:
+    def calculate_delivery_cost(self, user=None) -> float:
         shopping_bag_total_price = ShoppingBag.objects.calculate_total_price(user)
 
         delivery_cost = Decimal(30)
@@ -46,39 +48,37 @@ class ExpressHomeDeliveryStrategy(DeliveryStrategy):
 
         return delivery_cost
 
-    def get_delivery_details(self, user) -> str:
-        pass
-        # delivery_instructions = input("Please enter any delivery instructions (e.g., leave at the door, etc.): ")
-        # preferred_delivery_time = input("Preferred delivery time (e.g., morning, afternoon, etc.): ")
+    def calculate_delivery_due_date(self) -> str:
+        return now().date() + timedelta(days=1)
 
 
 class RegularHomeDeliveryStrategy(DeliveryStrategy):
-    def calculate_delivery_cost(self, user) -> float:
+    def calculate_delivery_cost(self, user=None) -> float:
         shopping_bag_total_price = ShoppingBag.objects.calculate_total_price(user)
 
-        delivery_cost = Decimal(20)
+        delivery_cost = Decimal(10)
 
         if shopping_bag_total_price > Decimal(150_000):
             delivery_cost = Decimal(0)
 
         return delivery_cost
 
-    def get_delivery_details(self, user) -> str:
-        return "Regular home delivery within 3-5 business days."
+    def calculate_delivery_due_date(self) -> str:
+        return now().date() + timedelta(days=7)
 
 
 class DeliveryContext:
     def __init__(self, strategy: DeliveryStrategy) -> None:
         self.strategy = strategy
 
-    def set_delivery_method(self, user):
-        cost = self.strategy.calculate_delivery_cost()
-        details = self.strategy.get_delivery_details(user)
+    def get_delivery_details(self, user=None):
+        cost = self.strategy.calculate_delivery_cost(user)
+        details = self.strategy.calculate_delivery_due_date()
 
         return {"delivery_cost": cost, "shipping_details": details}
 
 
-def execute_setting_delivery_method(method, user):
+def execute_context(method, user=None):
     strategies = {
         DeliveryMethod.STORE_PICKUP: StorePickupStrategy(),
         DeliveryMethod.EXPRESS_HOME: ExpressHomeDeliveryStrategy(),
@@ -86,4 +86,4 @@ def execute_setting_delivery_method(method, user):
     }
 
     context = DeliveryContext(strategy=strategies[method])
-    return context.set_delivery_method(user)
+    return context.get_delivery_details(user)
