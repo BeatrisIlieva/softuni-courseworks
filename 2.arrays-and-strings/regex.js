e94ec9d7a59d8dbe4243fbdc73d2b0c586f06794
Beatris Ilieve <beatrisilieve@icloud.com>
2025-01-05 11:31:12 +0200

    added

--- 2.arrays-and-strings/regex.js
+++ 2.arrays-and-strings/regex.js
@@ -1,7 +1,17 @@
-let text = 'I am JavaScript developer';
+
+let text = 'I am JavaScript developer JavaScript';
 
 // RegExp literal
-let pattern = /JavaScript/;
+let pattern = /JavaScript/; // case sensitive
+let patternInsensitive = /javascript/ig; // case insensitive
 
 // RegExp function constructor
-let pattern2 = new RexExp('JavaScript');
+let pattern2 = new RegExp('JavaScript', 'i');
+
+// test pattern
+console.log(pattern.test(text));
+
+// exec pattern we need to call it again for the next occurrence
+// it knows where to start from after the first time
+console.log(patternInsensitive.exec(text));
+console.log(patternInsensitive.exec(text));
