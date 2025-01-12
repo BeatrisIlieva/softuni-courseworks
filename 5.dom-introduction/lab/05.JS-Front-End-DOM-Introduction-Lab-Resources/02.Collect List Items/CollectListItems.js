990e39ca23f2a012848acc301b966542f4508438
Beatris Ilieve <beatrisilieve@icloud.com>
2025-01-12 17:26:13 +0200

    added

--- 5.dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
+++ 5.dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,9 @@
 function extractText() {
-    // TODO
+    const listItemElements = document.querySelectorAll('#items li');
+    const textAreaElement = document.getElementById('result');
+
+    for (let item of listItemElements) {
+        console.log(item);
+        textAreaElement.textContent += item.textContent + '\n';
+    }
 }
--- 5.dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
+++ 5.dom-introduction/lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,10 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const firstNumberElement = document.getElementById('num1');
+    const secondNumberElement = document.getElementById('num2');
+    const sumElement = document.getElementById('sum');
+
+    const firstNumberValue = firstNumberElement.value;
+    const secondNumberValue = secondNumberElement.value;
+
+    sumElement.value = Number(firstNumberValue) + Number(secondNumberValue);
 }
