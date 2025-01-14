8475eb726b02797a4e12fd5d4468b393ca688c7c
Beatris Ilieve <beatrisilieve@icloud.com>
2025-01-14 19:20:57 +0200

    added

--- dom-manipulation-and-events/lab/01. List of Items_Ресурси/03. Add-Delete/add-delete.js
+++ dom-manipulation-and-events/lab/01. List of Items_Ресурси/03. Add-Delete/add-delete.js
@@ -1,3 +1,22 @@
 function addItem() {
-    // TODO
+    const listElement = document.getElementById('items');
+    const inputElement = document.getElementById('newItemText');
+
+    const input = inputElement.value;
+
+    const listItem = document.createElement('li');
+    listItem.textContent = input;
+    listElement.appendChild(listItem);
+
+    const deleteButtonElement = document.createElement('a');
+    deleteButtonElement.href = '#';
+    deleteButtonElement.textContent = '[Delete]';
+
+    deleteButtonElement.addEventListener('click', () => {
+        listItem.remove();
+    });
+
+    listItem.appendChild(deleteButtonElement);
+
+    inputElement.textContent = '';
 }
