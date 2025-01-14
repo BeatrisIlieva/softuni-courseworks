a601f193c233af4eec37730abb5d55662a2592c7
Beatris Ilieve <beatrisilieve@icloud.com>
2025-01-14 11:09:30 +0200

    added

--- 5.dom-introduction/exe/01. Subtraction_Ресурси/08. Generate Report/generate-report.js
+++ 5.dom-introduction/exe/01. Subtraction_Ресурси/08. Generate Report/generate-report.js
@@ -1,3 +1,25 @@
 function solve() {
-    //TODO
+    const tHeaderElements = document.querySelectorAll('table thead tr th');
+    const tBodyElements = document.querySelectorAll('table tbody tr');
+    const outputElement = document.getElementById('output');
+
+    const checkedInputElements = [...tHeaderElements]
+        .map((cell, i) => {
+            const inputElement = cell.querySelector('input[type="checkbox"]');
+
+            return { element: inputElement, name: inputElement.name, index: i };
+        })
+        .filter((object) => object.element.checked);
+    console.log(checkedInputElements);
+
+    const result = [...tBodyElements].map((row) => {
+        const dataElements = row.querySelectorAll('td');
+
+        return checkedInputElements.reduce((acc, curr) => {
+            acc[curr.name] = dataElements[curr.index].textContent;
+            return acc;
+        }, {});
+    });
+
+    outputElement.textContent = JSON.stringify(result);
 }
