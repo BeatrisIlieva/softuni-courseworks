c23d148920340484ad3edc9c19a8dadeebd86146
Beatris Ilieve <beatrisilieve@icloud.com>
2025-01-16 11:48:38 +0200

    added

--- 6.dom-manipulation-and-events/exe/01. Sections_Ресурси/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ 6.dom-manipulation-and-events/exe/01. Sections_Ресурси/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,52 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const encodeFormElement = document.getElementById('encode');
+    const decodeFormElement = document.getElementById('decode');
+
+    encodeFormElement.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const textAreaElement = e.target.querySelector('textarea');
+        const text = textAreaElement.value;
+
+        if (text === '') {
+            return;
+        }
+
+        const encodedText = text
+            .split('')
+            .map((char) => {
+                const decodedAsciiValue = char.charCodeAt(0) + 1;
+                return String.fromCharCode(decodedAsciiValue);
+            })
+            .join('');
+
+        const decodeFormElementTextArea = decodeFormElement.querySelector('textarea');
+
+        decodeFormElementTextArea.value = encodedText;
+
+        textAreaElement.value = '';
+    });
+
+    decodeFormElement.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const textAreaElement = e.target.querySelector('textarea');
+        const text = textAreaElement.value;
+
+        if (text === '') {
+            return;
+        }
+
+        const encodedText = text
+            .split('')
+            .map((char) => {
+                const decodedAsciiValue = char.charCodeAt(0) - 1;
+                return String.fromCharCode(decodedAsciiValue);
+            })
+            .join('');
+
+        textAreaElement.value = encodedText;
+    });
 }
