df1bfc6d6d175154101d85f7150a1a9f706153ae
Beatris Ilieve <beatrisilieve@icloud.com>
2025-01-19 17:54:20 +0200

    added

--- 7.http-and-rest/01.Resources/02.Bus-Schedule/app.js
+++ 7.http-and-rest/01.Resources/02.Bus-Schedule/app.js
@@ -1,15 +1,59 @@
 function solve() {
+    // const infoElement = document.querySelector('#info .info');
+    // const departInputElement = document.getElementById('depart');
+    // const arriveInputElement = document.getElementById('arrive');
+
+    let currentStop = 'depot';
+    let nextStop = 'depot';
+
     function depart() {
-        // TODO: 
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${currentStop}`)
+            .then(response => response.json())
+            .then(result => {
+                const infoElement = document.querySelector('#info .info');
+                const departInputElement = document.getElementById('depart');
+                const arriveInputElement = document.getElementById('arrive');
+                nextStop = result.next;
+
+                infoElement.textContent = `Next stop ${result.name}`;
+                toggleButton(departInputElement);
+                toggleButton(arriveInputElement);
+            })
+            .catch(() => {
+                infoElement.textContent = 'Error';
+
+                toggleButton(departInputElement);
+            });
     }
 
     async function arrive() {
-        // TODO:
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${currentStop}`)
+            .then(response => response.json())
+            .then(result => {
+                const infoElement = document.querySelector('#info .info');
+                const departInputElement = document.getElementById('depart');
+                const arriveInputElement = document.getElementById('arrive');
+                currentStop = nextStop;
+
+                infoElement.textContent = `Arriving at ${result.name}`;
+
+                toggleButton(departInputElement);
+                toggleButton(arriveInputElement);
+            })
+            .catch(() => {
+                infoElement.textContent = 'Error';
+
+                toggleButton(arriveInputElement);
+            });
+    }
+
+    function toggleButton(button) {
+        button.disabled = !button.disabled;
     }
 
     return {
         depart,
-        arrive
+        arrive,
     };
 }
 
