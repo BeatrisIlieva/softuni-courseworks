8b300b925da64bd0fa5a53d743a611c4b029b858
Beatris Ilieve <beatrisilieve@icloud.com>
2025-01-23 13:28:13 +0200

    added

--- card-slider/script.js
+++ card-slider/script.js
@@ -1,70 +1,9 @@
-// const nextButtonElements = document.querySelectorAll('.next');
-// const previousButtonElements = document.querySelectorAll('.previous');
+const carouselElements = document.querySelectorAll('.carousel');
 
-// nextButtonElements.forEach(button => {
-//     button.addEventListener('click', e => {
-//         const button = e.target;
-
-//         button.style.color = 'red';
-
-        // const carouselElement = button.closest('.slider');
-
-        // const ulElement = parentElement.querySelector('ul');
-
-        // const liElements = Array.from(ulElement.querySelectorAll('li'));
-
-//         return updateDisplayProperty(liElements);
-//     });
-// });
-
-// previousButtonElements.forEach(button => {
-//     button.addEventListener('click', e => {
-//         const button = e.target;
-
-//         button.style.color = 'red';
-
-//         const parentElement = button.closest('.slider');
-
-//         const ulElement = parentElement.querySelector('ul');
-
-//         const liElements = Array.from(ulElement.querySelectorAll('li'));
-
-//         return updateDisplayPropertyBackwards(liElements);
-//     });
-// });
-
-function displayNextElement(liElements) {
-    for (let i = 0; i < liElements.length - 1; i++) {
-        let currentElement = liElements[i];
-
-        if (window.getComputedStyle(currentElement).display === 'block') {
-            currentElement.style.display = 'none';
-            liElements[i + 1].style.display = 'block';
-
-            break;
-        }
-    }
-}
-
-function displayPreviousElement(liElements) {
-    for (let i = liElements.length - 1; i > 0; i--) {
-        let currentElement = liElements[i];
-
-        if (window.getComputedStyle(currentElement).display === 'block') {
-            currentElement.style.display = 'none';
-            liElements[i - 1].style.display = 'block';
-
-            break;
-        }
-    }
-}
-
-const sliderElements = document.querySelectorAll('.slider');
-
-sliderElements.forEach(sliderElement => {
+carouselElements.forEach(sliderElement => {
     sliderElement.addEventListener('click', e => {
-        const buttonElement = e.target;
         const carouselElement = e.currentTarget;
+        const buttonElement = e.target;
 
         const ulElement = carouselElement.querySelector('ul');
         const liElements = Array.from(ulElement.querySelectorAll('li'));
@@ -73,16 +12,41 @@ sliderElements.forEach(sliderElement => {
             const buttonId = buttonElement.id;
 
             return functionMapper[buttonId](liElements);
-            // if (buttonElement.id === 'next') {
-            //     return displayNextElement(liElements);
-            // } else if (buttonElement.id === 'previous') {
-            //     return displayPreviousElement(liElements);
-            // }
         }
     });
 
     const functionMapper = {
-        'next': (liElements) => displayNextElement(liElements),
-        'previous': (liElements) => displayPreviousElement(liElements)
-    }
+        next: liElements => displayNextElement(liElements),
+        previous: liElements => displayPreviousElement(liElements),
+    };
 });
+
+function displayNextElement(elements) {
+    for (let i = 0; i < elements.length; i++) {
+        let currentElement = elements[i];
+
+        if (window.getComputedStyle(currentElement).display === 'block') {
+            currentElement.style.display = 'none';
+
+            const nextIndex = (i + 1) % elements.length;
+            elements[nextIndex].style.display = 'block';
+
+            break;
+        }
+    }
+}
+
+function displayPreviousElement(elements) {
+    for (let i = elements.length - 1; i >= 0; i--) {
+        let currentElement = elements[i];
+
+        if (window.getComputedStyle(currentElement).display === 'block') {
+            currentElement.style.display = 'none';
+
+            const previousIndex = (i - 1 + elements.length) % elements.length;
+            elements[previousIndex].style.display = 'block';
+
+            break;
+        }
+    }
+}
