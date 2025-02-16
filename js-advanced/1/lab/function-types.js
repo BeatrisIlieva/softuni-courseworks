91ce44474ca690e1fcad306c69d6b158b95c94b0
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-16 16:25:06 +0200

    update

--- js-advanced/1/lab/function-types.js
+++ js-advanced/1/lab/function-types.js
@@ -8,11 +8,24 @@ const print2 = function (text) {
     console.log(text);
 };
 
-// Arrow Function
+// Arrow Function Statement Body
 const print3 = text => {
     console.log(text);
 };
 
+// Arrow function Expression Body
+
+const print4 = text => console.log(text);
+
 print1('some');
 print2('some');
 print3('some');
+print4('some');
+
+// Default Parameters
+
+function hello(name = 'guest'){
+    console.log(`Hi, ${name}`);
+}
+
+hello();
---
+++ js-advanced/1/lab/logical-comparison.js
@@ -0,0 +1,17 @@
+// When logical AND if the left side is truthy it will return the right value 
+console.log(true && false);
+// false
+
+console.log(true && 'Pesho');
+// Pesho
+
+// When logical AND if the left side is falsy it will return the left value 
+console.log(false && 'Pesho');
+// false
+
+
+// When logical OR if the left part if truthy it will return the left
+console.log(true || 'Pesho');
+
+// When logical OR if the left part if falsy it will return the right
+console.log(false || 'Pesho');
