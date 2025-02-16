bcc31a43fb6d1c985feaad98c6965a3baa076ffb
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-16 18:38:20 +0200

    update

--- js-advanced/2/array-destructuring.js
+++ js-advanced/2/array-destructuring.js
@@ -1,6 +1,11 @@
-const names = ['Pesho', 'Gosho', 'Stamat'];
+const names = ['Pesho', 'Gosho', 'Stamat', 'Mariyka'];
 
-//Array destructuring assignment
+// Array destructuring assignment
 const [firstName, secondName] = names;
 console.log(firstName);
 console.log(secondName);
+
+// Array destructuring assignment with rest operator
+const [fName, ...others] = names;
+console.log(fName);
+console.log(others);
