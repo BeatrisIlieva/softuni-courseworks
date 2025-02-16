8ef6ee338b0a81f875ff5a2f437b0d531fc491fd
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-16 18:32:47 +0200

    update

--- js-advanced/1/lab/logical-comparison.js
+++ js-advanced/1/lab/logical-comparison.js
@@ -12,6 +12,10 @@ console.log(false && 'Pesho');
 
 // When logical OR if the left part if truthy it will return the left
 console.log(true || 'Pesho');
+// true
 
 // When logical OR if the left part if falsy it will return the right
 console.log(false || 'Pesho');
+// Pesho
+
+console.log('Gosho' || 'Pesho');
---
+++ js-advanced/2/array-destructuring.js
@@ -0,0 +1,6 @@
+const names = ['Pesho', 'Gosho', 'Stamat'];
+
+//Array destructuring assignment
+const [firstName, secondName] = names;
+console.log(firstName);
+console.log(secondName);
