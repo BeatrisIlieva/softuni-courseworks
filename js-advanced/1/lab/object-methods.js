10ab9a05e2be17b453df56e232f1f5d29a2c2637
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-17 16:36:23 +0200

    update

---
+++ js-advanced/1/lab/object-methods.js
@@ -0,0 +1,21 @@
+let cat = {
+    sleep: function () {
+        console.log('Zzzz');
+    },
+};
+
+cat.sleep();
+
+let car = {
+    drive() {
+        console.log('Brrrum');
+    },
+};
+
+car.drive();
+
+let bird = {
+    fly: () => console.log('fly'),
+};
+
+bird.fly();
--- js-advanced/3/object.js
+++ js-advanced/3/object.js
@@ -11,3 +11,48 @@ const cat = {
 cat.yeeColor = 'Green';
 
 console.log(cat);
+
+const firstName = 'Pesho';
+let obj = {
+    firstName,
+    'last-name': 'Ivanov',
+    age: 20,
+};
+
+// access property with dot notation
+console.log(obj.firstName);
+
+// access property value with bracket notation
+console.log(obj['firstName']);
+
+// access property value with bracket notation using variable
+let dynamicPropertyName = 'firstName';
+console.log(obj[dynamicPropertyName]);
+
+console.log(obj['last-name']);
+
+// Object destructuring assignment
+let { name, age } = cat;
+console.log(name, age);
+
+// Rest operator
+let { isFemale, ...rest } = cat;
+
+console.log(isFemale, rest);
+
+const simpleObject = { first: 1, second: 2 };
+delete simpleObject.first;
+console.log(simpleObject);
+
+// Reference value
+let anotherObject = simpleObject;
+// we create a new variable that points to the same object in memory that simple object points to
+
+// use dynamic property name in object literal
+let propName = 'isEco';
+let house = {
+    color: 'Pink',
+    [propName]: true,
+};
+
+console.log(house);
