dc887bdc2fc7d2af3e57eed7c6c5f5703d110251
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-18 15:15:18 +0200

    update

--- js-front-end/1/lab/2.js
+++ js-front-end/1/lab/2.js
@@ -1,5 +1,5 @@
-function solve(name, age, grade){
+function solve(name, age, grade) {
     console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
 }
 
-solve('John', 15, 5.54678)
+solve('John', 15, 5.54678);
