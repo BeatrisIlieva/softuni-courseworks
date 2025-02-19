67c120514d800c2a00b6da50ad9e7353f053f7c5
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-19 15:17:22 +0200

    update

--- js-advanced/4/lab/04.JS-Advanced-DOM-Introduction-Lab-Resources/01.Edit Element/EditElement.js
+++ js-advanced/4/lab/04.JS-Advanced-DOM-Introduction-Lab-Resources/01.Edit Element/EditElement.js
@@ -1,3 +1,16 @@
-function editElement() {
-    // TODO
+// function editElement(element, match, replacer) {
+//     const elementTextContent = element.textContent;
+//     const updatedTextContent = elementTextContent.replaceAll(match, replacer);
+
+//     element.textContent = updatedTextContent;
+// }
+
+function editElement(element, match, replacer) {
+    let elementTextContent = element.textContent;
+
+    while (elementTextContent.includes(match)) {
+        elementTextContent = elementTextContent.replace(match, replacer);
+    }
+
+    element.textContent = elementTextContent;
 }
--- js-advanced/4/lab/04.JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
+++ js-advanced/4/lab/04.JS-Advanced-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,9 @@
 function extractText() {
-    // TODO
+    const itemsElements = document.querySelectorAll('#items li');
+    const itemsElementsAsArray = Array.from(itemsElements);
+    const resultElement = document.getElementById('result');
+    
+    const textContent = itemsElementsAsArray.map(element => element.textContent);
+
+    resultElement.textContent = textContent.join('\n');
 }
