0c72bdb6d937b73e55a69c539d9bf616d3010d42
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-19 15:30:59 +0200

    update

--- js-advanced/4/lab/04.JS-Advanced-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
+++ js-advanced/4/lab/04.JS-Advanced-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+    const tableEvenRowsElements = document.querySelectorAll('table tr:nth-child(even)');
+    const tableEvenRowsElementsArray = Array.from(tableEvenRowsElements);
+
+    for (let row of tableEvenRowsElementsArray) {
+        row.style.backgroundColor = 'teal';
+    }
 }
