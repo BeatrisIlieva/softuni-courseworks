3ebdc5209005808106a382a4f32b0e2a823aa59c
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-19 15:47:35 +0200

    update

--- js-advanced/4/lab/04.JS-Advanced-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
+++ js-advanced/4/lab/04.JS-Advanced-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,10 @@
-function extract(content) {
+function extract(elementId) {
+    const contentElement = document.getElementById(elementId);
+    const contentText = contentElement.textContent;
 
+    const pattern = /\(([^\(\)]+)\)/g;
+
+    const matches = Array.from(contentText.matchAll(pattern));
+
+    return matches.map(match => match[1]).join('; ');
 }
