6bca90157c04d41c652c44126a706117ecb3cb26
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-19 17:20:51 +0200

    update

--- js-advanced/4/exe/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ js-advanced/4/exe/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,32 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+    function onClick() {
+        const inputElement = document.getElementById('searchField');
+        let tdElements = document.querySelectorAll('tbody td');
+        let trElements = document.querySelectorAll('tbody tr');
 
-   }
+        trElements = Array.from(trElements);
+
+        for (let element of trElements) {
+            element.classList.remove('select');
+        }
+
+        tdElements = Array.from(tdElements);
+
+        const input = inputElement.value.toLowerCase();
+
+        for (let element of tdElements) {
+            let elementContent = element.textContent.toLowerCase();
+            let isMatched = elementContent.includes(input);
+
+            let parentElement = element.parentElement;
+
+            if (isMatched) {
+                parentElement.classList.add('select');
+            }
+        }
+
+        inputElement.value = '';
+    }
 }
