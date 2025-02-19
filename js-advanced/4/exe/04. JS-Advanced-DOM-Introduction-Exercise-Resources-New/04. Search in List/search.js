07655598cfde696fd13a1bcc623c847a2e1e18e7
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-19 17:01:48 +0200

    update

--- js-advanced/4/exe/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ js-advanced/4/exe/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,23 @@
 function search() {
-   // TODO
+    let townElements = document.querySelectorAll('#towns li');
+    const searchTextElement = document.getElementById('searchText');
+    const resultElement = document.getElementById('result');
+
+    const searchText = searchTextElement.value;
+
+    townElements = Array.from(townElements);
+    let count = 0;
+
+    for (let town of townElements) {
+        let townTextContent = town.textContent;
+        let townFound = townTextContent.includes(searchText);
+
+        count += townFound ? 1 : 0;
+
+        town.style.textDecoration = townFound ? 'underline' : 'none';
+        town.style.fontWeight = townFound ? 'bold' : 'normal';
+    }
+
+    searchTextElement.value = '';
+    resultElement.textContent = `${count} matches found`;
 }
