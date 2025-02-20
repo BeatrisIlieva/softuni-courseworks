0aeb4791e2b6d583b129786c701f0967ad47aac6
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-20 19:46:56 +0200

    update

--- js-advanced/5/lab/02. Delete-From-Table/app.js
+++ js-advanced/5/lab/02. Delete-From-Table/app.js
@@ -1,3 +1,23 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const inputElement = document.querySelector('input[name=email]');
+    const tdElements = document.querySelectorAll('#customers tbody tr td:nth-child(2)');
+    const resultElement = document.getElementById('result');
+
+    const input = inputElement.value;
+
+    const matchedEmail = Array.from(tdElements).find(element => {
+        const email = element.textContent;
+
+        return email === input;
+    });
+
+    if (matchedEmail) {
+        matchedEmail.parentElement.remove();
+        resultElement.textContent = 'Deleted.';
+    } else {
+        resultElement.textContent = 'Not found.';
+    }
+
+    inputElement.textContent = '';
+    inputElement.focus();
 }
