e5167e29041ffffdb7ced6203360e91929ed67af
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-20 20:06:45 +0200

    update

--- js-advanced/5/lab/04. Mouse-Gradient/app.js
+++ js-advanced/5/lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradientElement = document.getElementById('gradient');
+    const resultElement = document.getElementById('result');
+
+    gradientElement.addEventListener('mousemove', e => {
+        const gradientElementWidth = e.currentTarget.offsetWidth;
+
+        const mousePosition = e.offsetX;
+        const percentage = Math.floor((mousePosition / gradientElementWidth) * 100);
+
+        resultElement.textContent = percentage;
+    });
 }
