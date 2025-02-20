c86b7b95f9367f8f9e92118a665ca76a72f525f4
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-20 12:17:59 +0200

    update

--- js-advanced/4/exe/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ js-advanced/4/exe/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-  //TODO
+    const textAreaElement = document.getElementById('input');
+    const outputElement = document.getElementById('output');
+
+    const text = textAreaElement.value;
+
+    const pattern = /[^.]+(?:\.\s*|$)/g;
+
+    const matches = Array.from(text.matchAll(pattern));
+
+    const pElementsFragment = document.createDocumentFragment();
+    let pElement;
+
+    for (let i = 0; i < matches.length; i++) {
+        const sentence = matches[i];
+
+        if (i % 3 === 0) {
+            pElement = document.createElement('p');
+            pElementsFragment.appendChild(pElement);
+        }
+
+        pElement.textContent += sentence;
+    }
+
+    outputElement.appendChild(pElementsFragment);
 }
+
+
+
+
