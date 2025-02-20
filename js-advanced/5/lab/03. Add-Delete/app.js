f501076b6f815507757bceef30df85a70f703a4e
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-20 19:54:03 +0200

    update

--- js-advanced/5/lab/03. Add-Delete/app.js
+++ js-advanced/5/lab/03. Add-Delete/app.js
@@ -1,3 +1,24 @@
 function addItem() {
-    //TODO...
+    const listElement = document.getElementById('items');
+    const inputElement = document.getElementById('newItemText');
+
+    const input = inputElement.value;
+
+    const liElement = document.createElement('li');
+    liElement.textContent = input;
+
+    const deleteButton = document.createElement('a');
+    deleteButton.href = '#';
+    deleteButton.textContent = '[Delete]';
+
+    deleteButton.addEventListener('click', e => {
+        liElement.remove();
+    });
+
+    liElement.append(deleteButton);
+
+    listElement.append(liElement);
+
+    inputElement.value = '';
+    inputElement.focus();
 }
