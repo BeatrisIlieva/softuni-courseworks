fdb7d41f3179019f819ed597a0be6d0814260d16
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-21 10:05:40 +0200

    update

--- js-advanced/5/lab/06. Dynamic-Validation/app.js
+++ js-advanced/5/lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+    const inputElement = document.getElementById('email');
+
+    const pattern = /^[a-z]{1,}@[a-z]{1,}\.[a-z]{1,}$/;
+
+    inputElement.addEventListener('change', e => {
+        const userEmail = inputElement.value;
+
+        const isValid = pattern.test(userEmail);
+
+        if (!isValid) {
+            inputElement.classList.add('error');
+        } else {
+            inputElement.classList.remove('error');
+        }
+    });
 }
--- js-advanced/5/lab/07. Shopping-Cart/solution.js
+++ js-advanced/5/lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,42 @@
 function solve() {
-   //TODO...
+    const addButtonElements = document.querySelectorAll('.add-product');
+    const checkoutButtonElement = document.querySelector('.checkout');
+    const textAreaElement = document.querySelector('textarea');
+
+    const productData = {};
+
+    addButtonElements.forEach(button => button.addEventListener('click', addProductHandler));
+
+    function addProductHandler(e) {
+        const parentElement = e.currentTarget.parentElement.parentElement;
+        const productName = parentElement.querySelector('.product-title').textContent;
+        let productPrice = parentElement.querySelector('.product-line-price').textContent;
+
+        productPrice = Number(productPrice);
+
+        if (!productData[productName]) {
+            productData[productName] = 0;
+        }
+
+        productData[productName] += productPrice;
+
+        textAreaElement.value += `Added ${productName} for ${productPrice.toFixed(
+            2
+        )} to the cart.\n`;
+    }
+
+    checkoutButtonElement.addEventListener('click', e => {
+        const list = Object.keys(productData).join(', ');
+        const totalPrice = Object.values(productData).reduce((acc, curr) => acc + curr, 0);
+
+        const result = `You bought ${list} for ${totalPrice.toFixed(2)}.`;
+
+        textAreaElement.value += result;
+
+        addButtonElements.forEach(button => {
+            button.setAttribute('disabled', 'disabled');
+        });
+
+        e.currentTarget.setAttribute('disabled', 'disabled');
+    });
 }
