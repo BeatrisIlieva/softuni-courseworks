712eef822b5b89a514af93bad84507ffd6eee80c
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-22 10:50:42 +0200

    update

--- js-advanced/5/exe/03. Locked-Profile/app.js
+++ js-advanced/5/exe/03. Locked-Profile/app.js
@@ -1,3 +1,27 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const showMoreButtonElements = document.querySelectorAll('button');
+
+    showMoreButtonElements.forEach(button => {
+        button.addEventListener('click', e => {
+            const parentElement = button.parentElement;
+            const unlockRadio = parentElement.querySelector('input[value=lock]');
+
+            if (unlockRadio.checked) {
+                return;
+            }
+
+            const buttonText = button.textContent;
+            const hiddenContent = parentElement.querySelector('div');
+
+            if (buttonText === 'Show more') {
+                hiddenContent.style.display = 'block';
+
+                button.textContent = 'Hide it';
+            } else {
+                hiddenContent.style.display = 'none';
+
+                button.textContent = 'Show more';
+            }
+        });
+    });
 }
