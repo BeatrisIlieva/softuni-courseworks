5af5ee4524b6d6f43b42bfdd199c660f3c15c53e
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-22 20:34:33 +0200

    update

--- js-front-end/5/exe/04. Search in List/search-in-list.js
+++ js-front-end/5/exe/04. Search in List/search-in-list.js
@@ -1,3 +1,23 @@
 function solve() {
-   // TODO
+    const townItems = document.querySelectorAll('#towns li');
+    const inputElement = document.getElementById('searchText');
+    const resultElement = document.getElementById('result');
+
+    const input = inputElement.value;
+    let count = 0;
+
+    townItems.forEach(item => {
+        const content = item.textContent;
+        const isMatched = content.includes(input);
+
+        if (isMatched) count++;
+
+        item.style.textDecoration = isMatched ? 'underline' : 'none';
+        item.style.fontWeight = isMatched ? 'bold' : 'normal';
+    });
+
+    resultElement.textContent = `${count} matches found`;
+
+    inputElement.value = '';
+    inputElement.focus();
 }
