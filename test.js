71763de4d7b0bf10464524a1f839e882f6628dd1
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-22 10:30:00 +0200

    update

--- test.js
+++ test.js
@@ -1,23 +1,2 @@
-function solve(obj){
-    obj.a = 2
-    obj = {}
-    obj.b = 4
-    return obj
-}
-
-const obj = {a: 1, b: 3}
-const obj2 = solve(obj)
-console.log(Object.is(obj, obj2));
-console.log(obj);
-
-
-
-
-// function solveNum(num) {
-//     num = 2
-//     return num
-// }
-
-// const num1 = 1;
-// const num2 = solveNum(num1)
-// console.log(Object.is(num1, num2));
+const operand = '*';
+console.log(3 operand 3);
