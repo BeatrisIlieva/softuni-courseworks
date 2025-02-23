fec435ed1e5d79f6bc78fec412ed17ab4695eceb
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-23 20:03:40 +0200

    update

--- js-front-end/6/exe/06. Furniture/furniture.js
+++ js-front-end/6/exe/06. Furniture/furniture.js
@@ -1,5 +1,76 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-  //TODO
+    const generateButtonElement = document.querySelector('#input input[type=submit]');
+
+    generateButtonElement.addEventListener('click', e => generateFurniture(e));
+
+    function generateFurniture(e) {
+        e.preventDefault();
+
+        const textareaElement = document.querySelector('textarea');
+
+        const input = JSON.parse(textareaElement.value);
+
+        const tbodyElement = document.querySelector('tbody');
+
+        input.forEach(furniture => {
+            const { name, img, price, decFactor } = furniture;
+
+            const tdForImgElement = createBlockElement('td');
+            const imageElement = createImageElement(img);
+            appendChildElement(tdForImgElement, imageElement);
+
+            const tdForNameElement = createBlockElement('td');
+            const nameElement = createBlockElement('p');
+            nameElement.textContent = name;
+            appendChildElement(tdForNameElement, nameElement);
+
+            const tdForPriceElement = createBlockElement('td');
+            const priceElement = createBlockElement('p');
+            priceElement.textContent = price;
+            appendChildElement(tdForPriceElement, priceElement);
+
+            const tdForDecFactorElement = createBlockElement('td');
+            const decFactorElement = createBlockElement('p');
+            decFactorElement.textContent = decFactor;
+            appendChildElement(tdForDecFactorElement, decFactorElement);
+
+            const checkboxElement = createInputElement('checkbox');
+
+            const trElement = createBlockElement('tr');
+
+            trElement.append(
+                tdForImgElement,
+                tdForNameElement,
+                tdForPriceElement,
+                tdForDecFactorElement,
+                checkboxElement
+            );
+
+            tbodyElement.append(trElement);
+        });
+    }
+
+    function appendChildElement(parent, child) {
+        parent.append(child);
+    }
+
+    function createBlockElement(tagName) {
+        return document.createElement(tagName);
+    }
+
+    function createImageElement(content) {
+        const imageElement = document.createElement('img');
+        imageElement.src = content;
+
+        return imageElement;
+    }
+
+    function createInputElement(type) {
+        const inputElement = document.createElement('input');
+        inputElement.type = type;
+
+        return inputElement;
+    }
 }
