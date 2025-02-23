0d406291511e5103f73b07399c29e7bc277afbf1
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-23 19:22:48 +0200

    update

--- js-front-end/6/exe/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ js-front-end/6/exe/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,39 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const buttonElements = document.querySelectorAll('button');
+
+    buttonElements.forEach(button => button.addEventListener('click', e => clickHandler(e)));
+
+    function clickHandler(e) {
+        e.preventDefault();
+        const button = e.currentTarget;
+
+        const textareaElement = button.previousElementSibling;
+        const message = textareaElement.value;
+        textareaElement.value = '';
+
+        const parentElement = button.closest('form');
+        const parentElementId = parentElement.id;
+
+        functionMapper[parentElementId](message);
+    }
+
+    const functionMapper = {
+        encode: message => {
+            const textareaElement = document.querySelector('#decode textarea');
+            textareaElement.value = createMessage(message, 1);
+        },
+        decode: message => {
+            const textareaElement = document.querySelector('#decode textarea');
+            textareaElement.value = createMessage(message, -1);
+        }
+    };
+
+    function createMessage(message, delta) {
+        return message
+            .split('')
+            .map(char => String.fromCharCode(char.charCodeAt(0) + delta))
+            .join('');
+    }
 }
