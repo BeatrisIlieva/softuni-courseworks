7352e1fae4007c12f1d8a46b8232c711c5491bf9
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-24 19:00:15 +0200

    update

--- js-front-end/6/exe/09. JavaScript-Quiz/quiz.js
+++ js-front-end/6/exe/09. JavaScript-Quiz/quiz.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-  //TODO
+    const sectionElements = document.querySelectorAll('.question');
+    const resultsElement = document.getElementById('results');
+    const answerElements = document.querySelectorAll('.quiz-answer');
+
+    sectionElements.forEach(element => element.addEventListener('click', e => clickHandler(e)));
+
+    function clickHandler(e) {
+        let rightAnswersCount = 0;
+
+        const section = e.currentTarget;
+
+        if (e.target.tagName === 'LI') {
+            const nextSection = section.nextElementSibling;
+
+            if (nextSection && nextSection.tagName == 'SECTION') {
+                section.classList.add('hidden');
+
+                nextSection.classList.remove('hidden');
+                console.log(nextSection);
+            }
+        }
+    }
 }
