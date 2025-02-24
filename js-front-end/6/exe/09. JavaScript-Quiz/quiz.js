27cf69c925ff361f40e95ef26b01424bf93fc08d
Beatris Ilieve <beatrisilieve@icloud.com>
2025-02-24 19:52:39 +0200

    update

--- js-front-end/6/exe/09. JavaScript-Quiz/quiz.js
+++ js-front-end/6/exe/09. JavaScript-Quiz/quiz.js
@@ -2,25 +2,48 @@ document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
     const sectionElements = document.querySelectorAll('.question');
-    const resultsElement = document.getElementById('results');
-    const answerElements = document.querySelectorAll('.quiz-answer');
 
     sectionElements.forEach(element => element.addEventListener('click', e => clickHandler(e)));
+    let correctAnswersCount = 0;
+
+    const correctAnswers = [
+        'onclick',
+        'JSON.stringify()',
+        'A programming API for HTML and XML documents'
+    ];
 
     function clickHandler(e) {
-        let rightAnswersCount = 0;
-
         const section = e.currentTarget;
 
-        if (e.target.tagName === 'LI') {
+        const clickedElement = e.target;
+
+        if (clickedElement.tagName === 'LI') {
+            const selectedAnswer = clickedElement.textContent;
+            const isSelectedAnswerCorrect = correctAnswers.includes(selectedAnswer);
+
+            isSelectedAnswerCorrect && correctAnswersCount++;
+
             const nextSection = section.nextElementSibling;
-
-            if (nextSection && nextSection.tagName == 'SECTION') {
-                section.classList.add('hidden');
-
-                nextSection.classList.remove('hidden');
-                console.log(nextSection);
+            section.classList.add('hidden');
+            nextSection.classList.remove('hidden');
+            if (nextSection.tagName == 'DIV') {
+                displayResult();
             }
         }
+    }
+
+    function displayResult() {
+        const resultElement = document.getElementById('results');
+        let resultSentence = '';
+
+        if (correctAnswersCount === correctAnswers.length) {
+            resultSentence = 'You are recognized as top JavaScript fan!';
+        } else if (correctAnswersCount > 1 || correctAnswersCount === 0) {
+            resultSentence = `You have ${correctAnswersCount} right answers`;
+        } else {
+            resultSentence = `You have ${correctAnswersCount} right answer`;
+        }
+
+        resultElement.textContent = resultSentence;
     }
 }
