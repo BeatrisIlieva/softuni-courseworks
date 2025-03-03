e186c7da498dba18cfa4b62edb9f8d19e4612d57
Beatris Ilieve <beatrisilieve@icloud.com>
2025-03-03 15:34:32 +0200

    added more theory

---
+++ 04.objects-and-composition/05.object-composition.js
@@ -0,0 +1,51 @@
+/*
+    Object composition happens when we combine reference data types -> object inside objects,
+    arrays inside objects, objects inside arrays, arrays inside arrays.
+    Object composition allows us to reuse code. It is considered superior to OOP inheritance.
+
+    In object Composition, objects are described by what they DO, not by what they ARE.
+*/
+
+const student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 21,
+    courses: { 'JS Advanced': 6.0, 'Django Basics': 6.0 }
+};
+
+console.log(student.courses); // {JS Advanced: 6, Django Basics: 6}
+console.log(student.courses['JS Advanced']); // 6
+
+// Composing objects with behavior
+// Since currently this is undefined because it refers to the global object
+// We can conclude that the functions are meant to be composed
+// and used as methods of an object
+
+function print() {
+    console.log(`${this.name} is printing page.`);
+}
+
+function scan() {
+    console.log(`${this.name} is scanning page.`);
+}
+
+const printer = {
+    name: 'Printer',
+    print,
+};
+
+const scanner = {
+    name: 'Scanner',
+    scan,
+}
+
+const copier = {
+    name: 'Copier',
+    print,
+    scan,
+}
+
+printer.print(); // Printer is printing page.
+scanner.scan(); // Scanner is scanning page.
+copier.print(); // Copier is printing page.
+copier.scan(); // Copier is scanning page.
