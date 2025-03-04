0916b37f468a81c802fb7da6b348622b2111e1b0
Beatris Ilieve <beatrisilieve@icloud.com>
2025-03-04 13:08:21 +0200

    added more theory

--- 05.dom/01.global-context.js
+++ 05.dom/01.global-context.js
@@ -1,11 +1,12 @@
 // In the Browser the global object is 'window', in Node.js the global object is 'global'
+// 'this' always refers to an object
 
 console.log(global); // global {global: global, clearImmediate: ƒ, setImmediate: ƒ, clearInterval: ƒ, clearTimeout: ƒ, …}
 
 console.log(this); // {}
 
-// 'globalThis' is window in the Browser and 'global' in Node.js
+// 'globalThis' is 'window' in the Browser and 'global' in Node.js
 
 console.log(globalThis === global); // true
-console.log(globalThis === this);
+console.log(globalThis === this); // false
 
