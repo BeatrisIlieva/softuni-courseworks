c481191d9f79968cc8b3c0083d3a69d35192b877
Beatris Ilieve <beatrisilieve@icloud.com>
2025-03-04 15:17:55 +0200

    added more theory

--- 05.dom/live collections/script.js
+++ 05.dom/live collections/script.js
@@ -1,6 +1,21 @@
 /*
     When a change occurs in the DOM this change will be reflected in a LIVE collection.
+    HTML Collection is always LIVE.
     In a STATIC Collection, future changes are not being reflected.
+
+    STATIC Node List is returned only when we use the method querySelectorAll();
+    LIVE Node List is returned by childNodes;
+
+    querySelector() => returns the first matched element;
+    querySelectorAll() => returns a STATIC Node List;
+
+    getElementById() => returns a single element;
+
+    getElementsByTagName() => returns a LIVE HTML Collection
+    getElementsByClassName() => returns a LIVE HTML Collection
+
+    .childNodes => returns a LIVE Node List
+    .children => returns a LIVE HTML collection
 */
 
 const moviesListElement = document.getElementById('movies');
