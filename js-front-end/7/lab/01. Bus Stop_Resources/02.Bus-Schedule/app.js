36605df36e3f362db133a8c5d68d5048cf04ccef
Beatris Ilieve <beatrisilieve@icloud.com>
2025-03-12 19:12:51 +0200

    added solution

--- js-front-end/7/lab/01. Bus Stop_Resources/02.Bus-Schedule/app.js
+++ js-front-end/7/lab/01. Bus Stop_Resources/02.Bus-Schedule/app.js
@@ -1,11 +1,32 @@
 function solve() {
+    const departButtonElement = document.getElementById('depart');
+    const arriveButtonElement = document.getElementById('arrive');
+    const infoElement = document.querySelector('#info span');
+
+    const baseUrl = 'http://localhost:3030/jsonstore/bus/schedule';
+    let nextId = 'depot';
+    let nextName = '';
 
     function depart() {
-        console.log('Depart TODO...');
+        fetch(`${baseUrl}/${nextId}`)
+            .then(response => response.json())
+            .then(data => displayContent(data));
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoElement.textContent = `Arriving at ${nextName}`;
+
+        departButtonElement.removeAttribute('disabled');
+        arriveButtonElement.setAttribute('disabled', 'disabled');
+    }
+
+    function displayContent(data) {
+        infoElement.textContent = data.name;
+        nextId = data.next;
+        nextName = data.name;
+
+        departButtonElement.setAttribute('disabled', 'disabled');
+        arriveButtonElement.removeAttribute('disabled');
     }
 
     return {
