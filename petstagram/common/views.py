c974a75b3d0d9174e23cac19a0f1b2e4b3c13145
Beatris Ilieve <beatrisilieve@icloud.com>
2025-03-31 19:24:51 +0300

    update

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, resolve_url
+
+from petstagram.photos.models import Photo
+from petstagram.common.models import Like
+
+from pyperclip import copy
 
 
 def home_page(request):
+    all_photos = Photo.objects.all()
 
-    return render(request, 'common/home-page.html')
+    context = {
+        'photos': all_photos,
+    }
+
+    return render(request, 'common/home-page.html', context)
+
+
+def likes_functionality(request, photo_id: int):
+    liked_object = Like.objects.filter(to_photo_id=photo_id).first()
+
+    if liked_object:
+        liked_object.delete()
+    else:
+        like = Like(to_photo_id=photo_id)
+        like.save()
+
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
+
+
+def share_functionality(request, photo_id: int):
+    copy(request.META.get('HTTP_HOST') + resolve_url('photo-details', photo_id))
+
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
+    
--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
 
+from petstagram.pets.models import Pet
+
 
 def pet_add_page(request):
     return render(request, 'pets/pet-add-page.html')
 
 
-def pet_edit_page(request, username:str, pet_slug:str):
+def pet_edit_page(request, username: str, pet_slug: str):
     return render(request, 'pets/pet-edit-page.html')
 
 
-def pet_delete_page(request, username:str, pet_slug:str):
+def pet_delete_page(request, username: str, pet_slug: str):
     return render(request, 'pets/pet-delete-page.html')
 
 
-def pet_details_page(request, username:str, pet_slug:str):
-    return render(request, 'pets/pet-details-page.html')
+def pet_details_page(request, username: str, pet_slug: str):
+    pet = Pet.objects.get(slug=pet_slug)
+    all_photos = pet.photo_set.all()
+
+    context = {
+        'pet': pet,
+        'all_photos': all_photos,
+    }
+
+    return render(request, 'pets/pet-details-page.html', context)
