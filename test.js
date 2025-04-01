cb282a39f1b4abd49fb9748fb39e5e933d9fcab5
Beatris Ilieve <beatrisilieve@icloud.com>
2025-04-01 21:31:28 +0300

    updated folders

--- test.js
+++
@@ -1,9 +0,0 @@
-function mather(){
-    child()
-
-    function child(){
-        console.log("child")
-    }
-}
-
-mather()
--- test.py
+++
@@ -1,5 +0,0 @@
-mydict = {"A": 1}
-
-for key in mydict:
-    print(key)
-    print(mydict[key])
