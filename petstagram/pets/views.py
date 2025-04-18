48772ee77af85111e46c249a03a882d094dcd0ea
Beatris Ilieve <beatrisilieve@icloud.com>
2025-04-18 13:03:21 +0300

    feat: added edit pet functionality

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 
-from petstagram.pets.forms import PetAddForm
+from petstagram.pets.forms import PetAddForm, PetEditForm
 from petstagram.pets.models import Pet
 
 
@@ -33,7 +33,21 @@ def pet_details_page(request, username: str, pet_slug: str):
 
 
 def pet_edit_page(request, username: str, pet_slug: str):
-    return render(request, 'pets/pet-edit-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    form = PetEditForm(request.POST or None, instance=pet)
+    
+    if request.method == 'POST':
+        if form.is_valid:
+            form.save()
+            
+            return redirect('pet-details', username, pet_slug)
+    
+    context = {
+        'form': form,
+        'pet': pet,
+    }
+    
+    return render(request, 'pets/pet-edit-page.html', context)
 
 
 def pet_delete_page(request, username: str, pet_slug: str):
