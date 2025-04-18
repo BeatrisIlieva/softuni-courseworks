983be073371083db68c8e0dc075dc9c1e91af943
Beatris Ilieve <beatrisilieve@icloud.com>
2025-04-18 11:24:06 +0300

    feat: added like and share functionality

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -1,4 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, resolve_url
+from pyperclip import copy
+
+from petstagram.common.models import Like
+from petstagram.photos.models import Photo
+
 
 def home_page(request):
-    return render(request, template_name='common/home-page.html')
+    all_photos = Photo.objects.all()
+
+    context = {
+        'photos': all_photos
+    }
+
+    return render(request, 'common/home-page.html', context)
+
+
+def likes_functionality(request, photo_id: int):
+    liked_object = Like.objects.filter(
+        to_photo_id=photo_id
+    ).first()
+    
+    if liked_object:
+        liked_object.delete()
+        
+    else:
+        like = Like(to_photo_id=photo_id)
+        like.save()
+        
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
+
+
+def share_functionality(request, photo_id: int):
+    copy(request.META.get('HTTP_HOST') + resolve_url('photo-details', photo_id))
+    
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
+    
