046cae13c7e40b763c7fefbaaa9d7db9fb000663
Beatris Ilieve <beatrisilieve@icloud.com>
2025-04-18 14:44:12 +0300

    feat: added search functionality

--- petstagram/common/forms.py
+++ petstagram/common/forms.py
@@ -12,3 +12,14 @@ class CommentForm(forms.ModelForm):
                 'placeholder': 'Add comment'
             })
         }
+
+
+class SearchForm(forms.Form):
+    pet_name = forms.CharField(
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Search by pet name...'
+            }
+        )
+    )
