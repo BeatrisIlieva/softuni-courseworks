a896559047ad1b462df349e6051d4d8863e86ecc
Beatris Ilieve <beatrisilieve@icloud.com>
2025-04-18 12:22:48 +0300

    feat: added comment ordering

--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 
+from petstagram.photos.models import Photo
+
 
 def photo_add_page(request):
     return render(request, template_name='photos/photo-add-page.html')
 
 
-def photo_details_page(request, pk:int):
-    return render(request, template_name='photos/photo-details-page.html')
+def photo_details_page(request, pk: int):
+    photo = Photo.objects.get(pk=pk)
+    likes = photo.like_set.all()
+    comments = photo.comment_set.all()
+
+    context = {
+        'photo': photo,
+        'likes': likes,
+        'comments': comments,
+    }
+    
+    return render(request, 'photos/photo-details-page.html', context)
 
 
-def photo_edit_page(request, pk:int):
-    return render(request, template_name='photos/photo-edit-page.html')
+def photo_edit_page(request, pk: int):
+    return render(request, 'photos/photo-edit-page.html')
