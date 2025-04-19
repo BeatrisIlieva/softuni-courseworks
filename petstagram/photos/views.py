68d1d1864f3e83cb655e420b684770b18d85fece
Beatris Ilieve <beatrisilieve@icloud.com>
2025-04-19 16:55:26 +0300

    fix: refactored FBV to CBV

--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import redirect, render
-
+from django.urls import reverse_lazy
+from django.views import generic as views
 from petstagram.common.forms import CommentForm
 from petstagram.photos.forms import PhotoAddForm, PhotoEditForm
 from petstagram.photos.models import Photo
+
+
+class PhotoAddPage(views.CreateView):
+    model = Photo
+    template_name = 'photos/photo-add-page.html'
+    form_class = PhotoAddForm
+    success_url = reverse_lazy('home')
 
 
 def photo_add_page(request):
@@ -21,6 +29,20 @@ def photo_add_page(request):
     return render(request, 'photos/photo-add-page.html', context)
 
 
+class PhotoDetailsPage(views.DetailView):
+    model = Photo
+    template_name = 'photos/photo-details-page.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context['likes'] = self.object.like_set.all()
+        context['comments'] = self.object.comment_set.all()
+        context['comment_form'] = CommentForm()
+        
+        return context
+
+
 def photo_details_page(request, pk: int):
     photo = Photo.objects.get(pk=pk)
     likes = photo.like_set.all()
@@ -36,7 +58,14 @@ def photo_details_page(request, pk: int):
 
     return render(request, 'photos/photo-details-page.html', context)
 
-
+class PhotoEditPage(views.UpdateView):
+    model = Photo
+    template_name  = 'photos/photo-edit-page.html'
+    form_class = PhotoEditForm
+    
+    def get_success_url(self):
+        return reverse_lazy('photo-details', kwargs={'pk': self.object.pk})
+    
 def photo_edit_page(request, pk: int):
     photo = Photo.objects.get(pk=pk)
     form = PhotoEditForm(request.POST or None, instance=photo)
