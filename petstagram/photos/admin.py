abfb6c8c29ebff1a9fcfdb08cfaa1bde06d1ec3d
Beatris Ilieve <beatrisilieve@icloud.com>
2025-04-20 16:46:43 +0300

    feat: added photo model

--- petstagram/photos/admin.py
+++ petstagram/photos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from petstagram.photos.models import Photo
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    pass
--- petstagram/photos/models.py
+++ petstagram/photos/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
-# Create your models here.
+from petstagram.pets.models import Pet
+from petstagram.photos.validators import FileSizeValidator
+
+
+class Photo(models.Model):
+    MAX_FILE_SIZE = 5
+    DESCRIPTION_MIN_LENGTH = 10
+    DESCRIPTION_MAX_LENGTH = 300
+
+    photo = models.ImageField(
+        validators=[
+            FileSizeValidator(MAX_FILE_SIZE),
+        ],
+    )
+
+    description = models.TextField(
+        validators=[
+            MinLengthValidator(DESCRIPTION_MIN_LENGTH),
+            MaxLengthValidator(DESCRIPTION_MAX_LENGTH),
+        ],
+    )
+
+    location = models.CharField(
+        max_length=30,
+    )
+
+    tagged_pets = models.ManyToManyField(
+        to=Pet,
+    )
+
+    date_of_publication = models.DateField(
+        auto_now_add=True,
+    )
