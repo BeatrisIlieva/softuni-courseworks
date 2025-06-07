776e094a09855d41272b1bd6434ee472a937884a
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-07 19:15:58 +0300

    added solutions

---
+++ 02.exe/03.ts
@@ -0,0 +1,28 @@
+type CarBody = { material: string; state: string };
+type Tires = { airPressure: number; condition: string };
+type Engine = { horsepower: number; oilDensity: number };
+
+type Diagnostics = {
+    partName: string;
+    runDiagnostics(): string;
+};
+
+function carDiagnostics(
+    carBody: CarBody & Diagnostics,
+    tires: Tires & Diagnostics,
+    engine: Engine & Diagnostics
+): void {
+    console.log(carBody.runDiagnostics());
+    console.log(tires.runDiagnostics());
+    console.log(engine.runDiagnostics());
+}
+
+function runDiagnostics(this: Diagnostics): string {
+    return this.partName;
+}
+
+carDiagnostics(
+    { material: 'aluminum', state: 'scratched', partName: 'Car Body', runDiagnostics },
+    { airPressure: 30, condition: 'needs change', partName: 'Tires', runDiagnostics },
+    { horsepower: 300, oilDensity: 780, partName: 'Engine', runDiagnostics }
+);
