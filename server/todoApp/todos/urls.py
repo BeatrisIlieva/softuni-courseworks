e2881205f2268d25eaad84182031925325a8f475
Beatris Ilieve <beatrisilieve@icloud.com>
2025-06-18 19:24:13 +0300

    merged repos

--- server/todoApp/todos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from todoApp.todos.views import TodoListCreateApiView, CategoriesListView
-
-urlpatterns = [
-    path('', TodoListCreateApiView.as_view(), name='todo-list-create'),
-    path('categories/', CategoriesListView.as_view(), name='category-list')
-]
