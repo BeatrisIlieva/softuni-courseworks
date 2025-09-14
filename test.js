7ad18aac067551d020cd257c0bb637b5edcac95f
Beatris Ilieve <beatrisilieve@icloud.com>
2025-09-14 21:55:14 +0300

    added chatbot for exam preparation

--- test.js
+++
@@ -1,4 +0,0 @@
-list_1 = [1, 2]
-list_2 = [1, 2]
-
-console.log(list_1 === list_2, 'here')
--- test.py
+++
@@ -1,9 +0,0 @@
-set_1 = {(1, 2)}
-
-set_1.add((1, 3))
-set_2 = {(3, 4), (7, 8)}
-
-set_1.update(set_2)
-print(set_1)
-
-[print(' '.join([str(x) for x in el] for el in set_1))]
